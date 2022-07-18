7e7e3cfb9e3b76543987d3e41f9a61c1b93d7016
jnsprnw <6gqrbj@posteo.org>
2022-07-18 17:23:54 +0200

    Better country search
    Still buggy

--- src/lib/geography-selection/country/index.svelte
+++ src/lib/geography-selection/country/index.svelte
@@ -2,6 +2,7 @@
   import { getContext } from 'svelte';
   import VirtualList from '@sveltejs/svelte-virtual-list';
   import MiniSearch from 'minisearch';
+  import Fuse from 'fuse.js';
   import { CURRENT_GEOGRAPHY } from '$lib/../stores/store.js';
   import TileGroup from "$lib/helper/tiles/TileGroup.svelte";
   import RadioTile from "$lib/helper/tiles/RadioTile.svelte";
@@ -16,12 +17,62 @@
   })
 
   // Index all documents
-  miniSearch.addAll(countries)
+  miniSearch.addAll(countries);
+
+  const options = {
+    includeScore: true,
+    keys: ['label', 'uid'],
+    includeMatches: true
+  }
+
+  const fuse = new Fuse(countries, options)
 
   let term = '';
 
+  const defaultResults = countries.map((d) => ({ item: d }))
+
   // Search with default options
-  $: results = term === '' ? countries: miniSearch.search(term);
+  $: results = (term === '' ? defaultResults: fuse.search(term)).map(({ item, matches }) => {
+    let label = item.label;
+    if (matches) {
+      const match = matches.find(d => d.key === 'label');
+      if (match) {
+        label = '';
+        if (item.label === 'Albania') {
+          console.log(match.indices)
+        }
+        for (let i = 0; i < match.indices.length; i++) {
+          const [start, end] = match.indices[i]; // Get start and end of this match
+          const matchAtBeginning = start === 0 || end === 0;
+          if (!matchAtBeginning) {
+            label += `${item.label.substring(0, start)}`;
+          }
+          label += `<mark>${item.label.substring(start, end + 1)}</mark>`;
+          // if (matchAtBeginning) { // If the match is not at the beginning, Fuse.js returns an 0,0-Array at the beginning. So we skip that.
+          //   // Otherwise we add this part of the string as a marked string
+          //   label += `<mark>${item.label.substring(start, end + 1)}</mark>`;
+          // }
+
+          if (end !== item.label.length) { // If we are not at the end of the string
+            let nextStart = item.label.length; // We set the end to the end of the string and …
+            if (match.indices[i + 1]) { // test if we have another index coming next so that …
+              [nextStart] = match.indices[i + 1]; // we use that start instead of the end of the string
+            }
+            // Now, we add this string unmarked to the label.
+            // We need to add 1 to the start-index if the current match was not at the beginning 
+            label += `${item.label.substring(end + (matchAtBeginning ? 1 : 0), nextStart)}`;
+          }
+        }
+      }
+    }
+    return {
+      label,
+      item,
+      matches
+    }
+  });
+
+  // $: console.log(results)
 
   function setGeography (value) {
     CURRENT_GEOGRAPHY.set(value);
@@ -31,9 +82,9 @@
 <div class="selection-country-wrapper">
   <TileGroup legend="Countries" bind:selected={$CURRENT_GEOGRAPHY}>
     <input type="text" bind:value={term} placeholder="Search…" />
-    <VirtualList items={results} let:item height="200px">
-    {#each results as item}
-      <RadioTile value={item.uid}>{#if item.emoji}{item.emoji}{/if} {item.label}</RadioTile>
+    <VirtualList items={results} let:country height="200px">
+    {#each results as { item, label }}
+      <RadioTile value={item.uid}>{#if item.emoji}{item.emoji}{/if} <span title={item.label}>{@html label}</span></RadioTile>
     {/each}
     </VirtualList>
   </TileGroup>
@@ -42,6 +93,9 @@
 </div>
 
 <style lang="scss">
+  mark {
+    background-color: yellow;
+  }
   .selection-country-wrapper {
     display: grid;
     grid-template-columns: 1fr 2fr;
