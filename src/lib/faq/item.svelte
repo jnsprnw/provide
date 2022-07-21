8290f854d300eda8e5fb6a2c1f9bce6d1f0e82b9
jnsprnw <6gqrbj@posteo.org>
2022-07-21 19:39:14 +0200

    FAQ styling init
    fix #4
    #9

--- src/lib/faq/item.svelte
+++ src/lib/faq/item.svelte
@@ -1,6 +1,81 @@
-<dt>
-  <slot name="term">Beast of Bodmin</slot>
-</dt>
-<dd>
-  <slot name="description">A large feline inhabiting Bodmin Moor.</slot>
-</dd>
+<script>
+  // Adapted from by https://github.com/carbon-design-system/carbon-components-svelte/blob/master/src/Accordion/AccordionItem.svelte
+  /** Set to `true` to open the first accordion item */
+  export let open = false;
+  /** Set to `true` to disable the accordion item */
+  export let disabled = false;
+  /** Specify the ARIA label for the accordion item chevron icon */
+  export let iconDescription = "Expand/Collapse";
+  import { onMount, getContext } from "svelte";
+  // import Chevron from "../Icons/Chevron.svelte";
+  let initialDisabled = disabled;
+  // const ctx = getContext("Accordion");
+  // const unsubscribe = ctx.disableItems.subscribe((value) => {
+  //   if (!value && initialDisabled) return;
+  //   disabled = value;
+  // });
+  let animation = undefined;
+  onMount(() => {
+    return () => {
+      unsubscribe();
+    };
+  });
+</script>
+
+<div
+  itemscope
+  itemprop="mainEntity"
+  itemtype="https://schema.org/Question"
+  class="question"
+  class:accordion__item="{true}"
+  class:accordion__item--active="{open}"
+  class:accordion__item--disabled="{disabled}"
+  class:accordion__item--expanding="{animation === 'expanding'}"
+  class:accordion__item--collapsing="{animation === 'collapsing'}"
+  {...$$restProps}
+  on:animationend
+  on:animationend="{() => {
+    animation = undefined;
+  }}">
+    <button
+      type="button"
+      class:accordion__heading="{true}"
+      title="{iconDescription}"
+      aria-expanded="{open}"
+      disabled="{disabled}"
+      on:click
+      on:click="{() => {
+        open = !open;
+        animation = open ? 'expanding' : 'collapsing';
+      }}"
+      on:mouseover
+      on:mouseenter
+      on:mouseleave
+      on:keydown
+      on:keydown="{({ key }) => {
+        if (open && key === 'Escape') {
+          open = false;
+        }
+      }}"
+      itemprop="name"
+    >
+      <div class:accordion__title="{true}">
+        <slot name="term" />
+      </div>
+    </button>
+  <div
+    itemscope
+    itemprop="acceptedAnswer"
+    itemtype="https://schema.org/Answer"
+    class:accordion__content="{true}">
+    <div itemprop="text">
+      <slot name="description" />
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .question {
+    grid-column: 7 / span 6;
+  }
+</style>
--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -1,19 +1,25 @@
 <script context="module">
 	import { parse } from 'marked';
+	import { groupBy } from "lodash-es";
 
   export const load = async () => {
     const res = await fetch('https://provide-cms.herokuapp.com/api/faqs');
     const data = await res.json();
+    const questions = data.data.map((d) => {
+			const { question, answer } = d.attributes;
+			return {
+				question,
+				answer: parse(answer),
+				topic: 'Risks and warnings'
+			}
+		});
+
+		const groups = Object.entries(groupBy(questions, 'topic'));
+		console.log({ groups })
 
     return {
     	props: {
-    		questions: data.data.map((d) => {
-    			const { question, answer } = d.attributes;
-    			return {
-    				question,
-    				answer: parse(answer)
-    			}
-    		})
+    		questions: groups
     	}
     };
   }
@@ -29,22 +35,38 @@
 	<title>FAQ</title>
 </svelte:head>
 
-<h1>FAQ</h1>
+<div class="faq-header container">
+	<div class="wrapper">
+		<h1>FAQ</h1>
+	</div>
+</div>
 
-<dl>
-	{#each questions as { question, answer }}
-	<Item>
-		<span slot="term">
-			{ question }
-		</span>
-		<p slot="description">
-			{ @html answer }
-		</p>
-	</Item>
-	{/each}
-</dl>
+<div class="faq-content container">
+	<div class="wrapper" itemscope itemtype="https://schema.org/FAQPage">
+		{#each questions as [topic, list]}
+		<section class="section wrapper grid">
+			<h2 class="topic">{ topic }</h2>
+			{#each list as { question, answer }}
+			<Item>
+				<span slot="term">
+					{ question }
+				</span>
+				<p slot="description">
+					{ @html answer }
+				</p>
+			</Item>
+			{/each}
+		</section>
+		{/each}
+	</div>
+</div>
 
 <style lang="scss">
 	@import '../styles/global.scss';
 
+	.section {
+		.topic {
+			grid-column: 1 / span 6;
+		}
+	}
 </style>
