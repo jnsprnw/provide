97915f775488e92913f40aa34f8e5f3dbcb75c01
Flavio <flavio.gortana@posteo.net>
2022-08-02 12:03:43 +0200

    dataprocessing for dist chart

--- src/lib/explore-impacts/impact-analysis/index.svelte
+++ src/lib/explore-impacts/impact-analysis/index.svelte
@@ -1,19 +1,47 @@
 <script>
-  import { get } from "lodash-es";
+  import { get, value } from "lodash-es";
   import { IMPACT_TIME_DATA } from "$lib/../stores/impact-time.js";
   import { IMPACT_TIME_DISTRIBUTION_DATA } from "$lib/../stores/impact-time-distribution.js";
   import { IMPACT_GEO_DATA } from "$lib/../stores/impact-geo.js";
   import { CURRENT_INDICATOR_UID } from "$lib/../stores/store.js";
   import LineDistributionChart from "$lib/charts/LineDistributionChart.svelte";
+
+  $: distDataStatus = get($IMPACT_TIME_DISTRIBUTION_DATA, ["status"]);
+  $: distData = get($IMPACT_TIME_DISTRIBUTION_DATA, ["data"]);
+
+  function processDistData() {
+    const { yearStart, valueStart, yearStep, valueStep, data } = distData || {};
+
+    const mean = data?.mean.map((value, i) => {
+      const year = new Date(yearStart + yearStep * i, 0, 1);
+      return { year, value };
+    });
+
+    const distribution = data?.distribution.map((yearValues, i) => {
+      const year = new Date(yearStart + yearStep * i, 0, 1);
+      return yearValues.map((distribution, j) => ({
+        year,
+        value: valueStart + valueStep * j,
+        distribution,
+      }));
+    });
+
+    return { mean, distribution };
+  }
+
+  $: processedDistData = processDistData(distData);
 </script>
-<p>{ $CURRENT_INDICATOR_UID }</p>
+
+<p>{$CURRENT_INDICATOR_UID}</p>
 
 <h2>Impact Time</h2>
 {#each $IMPACT_TIME_DATA as scenario}
   {#if get(scenario, "status") === "success"}
     <p>
-    	Status: { get(scenario, "status") }<br />
-      Data sample: { JSON.stringify(get(scenario, ["data", "data", $CURRENT_INDICATOR_UID, 0])) }
+      Status: {get(scenario, "status")}<br />
+      Data sample: {JSON.stringify(
+        get(scenario, ["data", "data", $CURRENT_INDICATOR_UID, 0])
+      )}
     </p>
   {:else}
     <p>Status: {get(scenario, "status")}</p>
@@ -21,21 +49,21 @@
 {/each}
 
 <h2>Impact Time Distribution</h2>
-{ get($IMPACT_TIME_DISTRIBUTION_DATA, ["status"]) }
-{ JSON.stringify(get($IMPACT_TIME_DISTRIBUTION_DATA, ['data', 'data', 'distribution', 0])) }
+{get($IMPACT_TIME_DISTRIBUTION_DATA, ["status"])}
+{JSON.stringify(
+  get($IMPACT_TIME_DISTRIBUTION_DATA, ["data", "data", "distribution", 0])
+)}
 
 <h2>Impact Geo</h2>
-{ get($IMPACT_GEO_DATA, ["status"]) }
-{ JSON.stringify(get($IMPACT_GEO_DATA, ['data', 'data', 0])) }
+{get($IMPACT_GEO_DATA, ["status"])}
+{JSON.stringify(get($IMPACT_GEO_DATA, ["data", "data", 0]))}
 
-<!-- {#if $IMPACT_TIME_DISTRIBUTION_DATA?.status === "success"}
+{#if distDataStatus === "success"}
   <div class="distribution-chart">
-    <LineDistributionChart
-      mean={$IMPACT_TIME_DISTRIBUTION_DATA.mean}
-      distribution={$IMPACT_TIME_DISTRIBUTION_DATA.distribution}
-    />
+    <LineDistributionChart {...processedDistData} />
   </div>
-{/if} -->
+{/if}
+
 <style lang="scss">
   .distribution-chart {
     height: 400px;
--- src/stores/impact-time-distribution.js
+++ src/stores/impact-time-distribution.js
@@ -1,10 +1,22 @@
-import { writable, derived } from 'svelte/store';
+import { writable, derived } from "svelte/store";
 import { END_DISTRIBUTION } from "$lib/../config.js";
 import { handle } from "$lib/api/api.js";
-import { CURRENT_GEOGRAPHY_UID, CURRENT_SCENARIOS_UID, CURRENT_INDICATOR_UID } from "./store.js";
+import {
+  CURRENT_GEOGRAPHY_UID,
+  CURRENT_SCENARIOS_UID,
+  CURRENT_INDICATOR_UID,
+} from "./store.js";
 
 export const IMPACT_TIME_DISTRIBUTION_CACHE = writable({});
 
-export const IMPACT_TIME_DISTRIBUTION_DATA = derived([IMPACT_TIME_DISTRIBUTION_CACHE, CURRENT_GEOGRAPHY_UID, CURRENT_INDICATOR_UID, CURRENT_SCENARIOS_UID], async ([cache, $geo, $indicator, $scenarios], set) => {
-	set(handle(END_DISTRIBUTION, "get")[0]); // This endpoint only has one dimension.
-});
+export const IMPACT_TIME_DISTRIBUTION_DATA = derived(
+  [
+    IMPACT_TIME_DISTRIBUTION_CACHE,
+    CURRENT_GEOGRAPHY_UID,
+    CURRENT_INDICATOR_UID,
+    CURRENT_SCENARIOS_UID,
+  ],
+  async ([cache, $geo, $indicator, $scenarios], set) => {
+    set(handle(END_DISTRIBUTION, "get")[0]); // This endpoint only has one dimension.
+  }
+);
