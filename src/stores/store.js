5ea1072c252a651b784d7b44c288071fa1bdab93
jnsprnw <6gqrbj@posteo.org>
2022-08-02 20:11:33 +0200

    Indicator options added
    #26

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,5 +1,6 @@
 import { writable, derived } from 'svelte/store';
 import { get, compact, groupBy, keyBy, map, isUndefined, isEmpty } from "lodash-es";
+import { OPTIONS } from "$lib/../config.js";
 
 export const INDICATORS = writable([]);
 export const SECTORS = writable([]);
@@ -34,6 +35,30 @@ export const CURRENT_SCENARIOS_UID = derived(
 export const CURRENT_INDICATOR_UID = derived(
 	CURRENT_INDICATOR,
 	$indicator => get($indicator, 'uid')
+);
+
+export const CURRENT_INDICATOR_OPTIONS_SELECTION = writable({});
+
+export const CURRENT_INDICATOR_OPTIONS = derived(
+	CURRENT_INDICATOR,
+	$indicator => {
+		const defs = {};
+		const list = compact(get($indicator, 'options', []).map(([option, def]) => {
+			defs[option] = def; // Setting the default value;
+			const options = get(OPTIONS, option);
+			if (!options) {
+				console.warn(`Option ${option} was not found. Please check the config file.`);
+				return false;
+			}
+			return {
+				...options,
+				key: option // TODO: Maybe we should write that directly into the options so we safe some time here.
+			};
+		}))
+		CURRENT_INDICATOR_OPTIONS_SELECTION.update(old => ({ ...old, ...defs })); // Updating the current option selection with the default values.
+		// TODO: Maybe we should overwrite the previously selection options with the new default values.
+		return list;
+	}
 );
 
 export const IMPACT_TIME_DATA = writable({});
