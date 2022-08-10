ef2c48d9cd869fc16062b59aa35d792a4c963cd1
jnsprnw <6gqrbj@posteo.org>
2022-08-10 10:16:03 +0200

    Align online and offline meta data

--- src/routes/api/meta.js
+++ src/routes/api/meta.js
@@ -1,9 +1,7 @@
 import { keyBy, get as take } from "lodash-es";
 // Will be loaded externally later
-import { GEOGRAPHY_TYPES, CONTINENTS, ADMIN_0, ADMIN_1, CITIES, ICONIC_REGIONS, SCENARIOS, UNITS, SECTORS, INDICATORS } from '$lib/../config.js';
+import { GEOGRAPHY_TYPES, ADMIN_0, ADMIN_1, CITIES, ICONIC_REGIONS, SCENARIOS, UNITS, SECTORS, INDICATORS } from '$lib/../config.js';
 import emojisCountries from '$lib/../data/emojis-countries.json';
-
-const continentDictionary = keyBy(CONTINENTS, 'uid');
 
 export function get() {
   return {
@@ -12,11 +10,9 @@ export function get() {
       continents: CONTINENTS,
       admin0: ADMIN_0.map((country) => {
         const emoji = take(emojisCountries, country.uid);
-        const continent = take(continentDictionary, [country.region, 'label'], country.region);
         return {
           ...country,
-          emoji,
-          continent
+          emoji
         }
       }),
       admin1: ADMIN_1,
