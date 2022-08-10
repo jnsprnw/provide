f0b30c47c8ce60bf8dca75ae6f599bf89d7cc0e2
jnsprnw <6gqrbj@posteo.org>
2022-08-10 15:01:01 +0200

    Fix API request

--- src/routes/api/meta.js
+++ src/routes/api/meta.js
@@ -1,6 +1,6 @@
 import { keyBy, get as take } from "lodash-es";
 // Will be loaded externally later
-import { GEOGRAPHY_TYPES, ADMIN_0, ADMIN_1, CITIES, ICONIC_REGIONS, SCENARIOS, UNITS, SECTORS, INDICATORS } from '$lib/../config.js';
+import { GEOGRAPHY_TYPES, CONTINENTS, ADMIN_0, ADMIN_1, CITIES, ICONIC_REGIONS, SCENARIOS, UNITS, SECTORS, INDICATORS } from '$lib/../config.js';
 import emojisCountries from '$lib/../data/emojis-countries.json';
 
 export function get() {
