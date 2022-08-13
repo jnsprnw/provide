7928ca12792c04caa1d04b7802d00d290661255f
jnsprnw <6gqrbj@posteo.org>
2022-08-13 21:39:19 +0200

    Use chart description helper

---
+++ src/lib/helper/chart-description/scenarioList.svelte
@@ -0,0 +1,22 @@
+<script>
+  import {
+    CURRENT_SCENARIOS,
+  } from '$lib/../stores/store';
+  import { formatObjArr } from '$lib/utils.js';
+  export let scenarios = $CURRENT_SCENARIOS;
+
+  $: scenarioList = formatObjArr(scenarios, 'label');
+</script>
+
+{#each scenarioList as { type, value }}
+  {#if type === 'element'}
+    <em
+      class="text-scenario-label"
+      style={`text-decoration-color: ${value.color}`}>
+      { value.label }
+    </em>
+  {:else}
+    { value }
+  {/if}
+{/each}
+scenario{#if scenarioList.length > 1}s{/if}
