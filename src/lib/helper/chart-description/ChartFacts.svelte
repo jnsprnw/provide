233521c126aff99d5410a2eb77771980a047ebf2
jnsprnw <6gqrbj@posteo.org>
2022-08-17 12:28:35 +0200

    Case sensitive

---
+++ src/lib/helper/chart-description/ChartFacts.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let direction;
+</script>
+
+<dl class="chart-info-facts" class:chart-info-facts--horizontal={direction === 'horizontal'}>
+  <slot />
+</dl>
+
+<style lang="scss">
+  .chart-info-facts {
+    color: var(--color-text-weaker);
+    font-size: var(--font-size-large-s);
+    line-height: var(--font-line-height-tightest);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: var(--size-spacer-large-xxxs) var(--size-spacer-large-xs);
+
+    &.chart-info-facts--horizontal {
+      grid-auto-flow: column;
+      grid-template-rows: auto auto;
+      grid-gap: var(--size-spacer-large-xxxs) var(--size-spacer-large-xs);
+    }
+  }
+</style>
---
+++ src/lib/helper/chart-description/ScenarioList.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { CURRENT_SCENARIOS } from '$lib/../stores/store';
+  import { formatObjArr } from '$lib/utils.js';
+  export let scenarios = $CURRENT_SCENARIOS;
+  import ScenarioSingle from './ScenarioSingle.svelte';
+
+  $: scenarioList = formatObjArr(scenarios, 'label');
+</script>
+
+{#each scenarioList as { type, value }}
+  {#if type === 'element'}
+    <ScenarioSingle scenario={value} />
+  {:else}
+    {value}
+  {/if}
+{/each}
+scenario{#if scenarioList.length > 1}s{/if}
