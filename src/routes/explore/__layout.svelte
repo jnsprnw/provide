7fd3d6dfd1e508e95ff692ec2a0ed97fe4fc05a2
jnsprnw <6gqrbj@posteo.org>
2022-08-18 09:12:42 +0200

    remove url params once applied

--- src/routes/explore/__layout.svelte
+++ src/routes/explore/__layout.svelte
@@ -38,19 +38,26 @@
   INDICATOR_PARAMETERS.set(meta.indicatorParameters);
   UNITS.set(meta.units);
 
-  $: params = parse(new URL($page.url).search.replace(/^\?/, ''));
+  $: currentURL = new URL($page.url);
+
+  $: params = parse(currentURL.search.replace(/^\?/, ''));
 
   $: {
     const { indicator, geography, scenario } = params;
+    let newUrl = currentURL;
     if (indicator) {
+      newUrl.searchParams.delete('indicator');
       CURRENT_INDICATOR_UID.set(indicator);
     }
     if (geography) {
+      newUrl.searchParams.delete('geography');
       CURRENT_GEOGRAPHY_UID.set(geography);
     }
     if (scenario) {
+      newUrl.searchParams.delete('scenario');
       CURRENT_SCENARIOS_UID.set([scenario]);
     }
+    window.history.replaceState(null, null, newUrl.href);
   }
 
   $: currentPath = $page.routeId || '';
