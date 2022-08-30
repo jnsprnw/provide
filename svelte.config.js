0c4ff7aec34b9139845823d03e9d81dc97fb39b4
Flavio <flavio.gortana@posteo.net>
2022-08-30 12:20:21 +0200

    added static export build task which is not working

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapterNetlify from '@sveltejs/adapter-netlify';
+import adapterStatic from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 import path from 'path';
+
+const isStatic = process.env.BUILD_ENV === 'static';
+const adapter = isStatic ? adapterStatic : adapterNetlify;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: sveltePreprocess(),
   kit: {
     adapter: adapter(),
+    prerender: {
+      default: isStatic,
+    },
     vite: {
       resolve: {
         alias: {
