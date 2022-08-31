382c3e259d4bb7b4866c9b6de136ea5741ac77af
jnsprnw <6gqrbj@posteo.org>
2022-08-31 16:05:45 +0200

    Data Processing section added

--- src/routes/methodology.svelte
+++ src/routes/methodology.svelte
@@ -5,26 +5,47 @@
 
   export const load = async ({ fetch }) => {
     const data = await loadFromStrapi('technical-documentation', fetch);
-    const { Models, Scenarios, ModelsIntro, ScenariosIntro } = data.attributes;
+    const { Models, Scenarios, ModelsIntro, ScenariosIntro, DataProcessing, DataProcessingIntro } = data.attributes;
 
     return {
       props: {
-        modelsIntro: parse(ModelsIntro),
+        modelsIntro: parse(ModelsIntro || ''),
         models: Models.map(({ Title, Description, Link, Label }) => {
+          let description = undefined;
+          if (Description) {
+            description = parse(Description);
+          }
           return {
             title: Title,
-            slug: kebabCase(Title),
-            description: parse(Description),
+            slug: kebabCase(Label || Title),
+            description,
             link: Link,
             label: Label || Title
           };
         }),
-        scenariosIntro: parse(ScenariosIntro),
+        scenariosIntro: parse(ScenariosIntro || ''),
         scenarios: Scenarios.map(({ Title, Description, Label }) => {
+          let description = undefined;
+          if (Description) {
+            description = parse(Description);
+          }
           return {
             title: Title,
-            slug: kebabCase(Title),
-            description: parse(Description),
+            slug: kebabCase(Label || Title),
+            description,
+            label: Label || Title
+          };
+        }),
+        dataProcessingIntro: parse(DataProcessingIntro || ''),
+        dataProcessing: DataProcessing.map(({ Title, Description, Label }) => {
+          let description = undefined;
+          if (Description) {
+            description = parse(Description);
+          }
+          return {
+            title: Title,
+            slug: kebabCase(Label || Title),
+            description,
             label: Label || Title
           };
         }),
@@ -40,6 +61,8 @@
   export let models;
   export let scenarios;
   export let scenariosIntro;
+  export let dataProcessing;
+  export let dataProcessingIntro;
 </script>
 
 <svelte:head>
@@ -51,22 +74,42 @@
     <h1 class="title">Methodology</h1>
     <nav>
       <ul class="nav-inpage subcategories">
+        {#if modelsIntro || models.length}
         <li>
           <a href="#models" class="nav-headline-section">Models</a>
+          {#if models.length}
           <ul>
-            {#each models as { title, slug, label }}
+            {#each models as { slug, label }}
               <li><a href={`#${slug}`}>{ label }</a></li>
             {/each}
           </ul>
+          {/if}
         </li>
+        {/if}
+        {#if scenariosIntro || scenarios.length}
         <li>
           <a href="#scenarios" class="nav-headline-section">Scenarios</a>
+          {#if scenarios.length}
           <ul>
-            {#each scenarios as { title, slug, label }}
+            {#each scenarios as { slug, label }}
               <li><a href={`#${slug}`}>{ label }</a></li>
             {/each}
           </ul>
+          {/if}
         </li>
+        {/if}
+        {#if dataProcessingIntro || dataProcessing.length}
+        <li>
+          <a href="#data-processing" class="nav-headline-section">Data Processing</a>
+          {#if dataProcessing.length}
+          <ul>
+            {#each dataProcessing as { slug, label }}
+              <li><a href={`#${slug}`}>{ label }</a></li>
+            {/each}
+          </ul>
+          {/if}
+        </li>
+        {/if}
       </ul>
     </nav>
   </div>
@@ -74,33 +117,51 @@
 
 <div class="technical-documentation-content content-content container">
   <div class="wrapper content-layout">
+    {#if modelsIntro || models.length}
     <section>
       <header>
         <h2 id="models" class="headline-section">Models</h2>
       </header>
       <div class="text">
-        {@html modelsIntro}
+        {#if modelsIntro}{@html modelsIntro}{/if}
         {#each models as { title, description, link, slug }}
           <h3 id={slug} class="headline-paragraph">{title}</h3>
-          {@html description}
+          {#if description}{@html description}{/if}
           {#if link}
           <a href={link} class="link">More information about {title}</a>
           {/if}
         {/each}
       </div>
     </section>
+    {/if}
+    {#if scenariosIntro || scenarios.length}
     <section>
       <header>
         <h2 id="scenarios" class="headline-section">Scenarios</h2>
       </header>
       <div class="text">
-        {@html scenariosIntro}
+        {#if scenariosIntro}{@html scenariosIntro}{/if}
         {#each scenarios as { title, description, slug }}
-          <h3 id={slug} class="headline-paragraph">{title}</h3>
-          {@html description}
+          <h3 id={slug} class="headline-paragraph">{ title }</h3>
+          {#if description}{@html description}{/if}
         {/each}
       </div>
     </section>
+    {/if}
+    {#if dataProcessingIntro || dataProcessing.length}
+    <section>
+      <header>
+        <h2 id="data-processing" class="headline-section">Data Processing</h2>
+      </header>
+      <div class="text">
+        {#if dataProcessingIntro}{@html dataProcessingIntro}{/if}
+        {#each dataProcessing as { title, description, slug }}
+          <h3 id={slug} class="headline-paragraph">{ title }</h3>
+          {#if description}{@html description}{/if}
+        {/each}
+      </div>
+    </section>
+    {/if}
   </div>
 </div>
 
