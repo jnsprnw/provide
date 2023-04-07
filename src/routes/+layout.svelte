20c6c018f35db4e8a75954818de5ec663d7fc709
jnsprnw <6gqrbj@posteo.org>
2023-04-07 12:04:33 +0200

    Prevent error message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
     evaluate: /\{\{=(.+?)\}\}/g,
   };
 
-  $: isEmbed = $page.route.id.startsWith('/embed');
+  $: isEmbed = ($page.route.id || '').startsWith('/embed');
 </script>
 
 <HtmlHead />
