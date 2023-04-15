dbbdaddf3ad30675dca116d4e774e38c7545d941
jnsprnw <6gqrbj@posteo.org>
2023-04-15 16:02:28 +0200

    Clean up
    #98

--- src/routes/(embed)/download/impact-time/+page.svelte
+++ src/routes/(embed)/download/impact-time/+page.svelte
@@ -1,10 +1,5 @@
 <script>
-  import { page } from '$app/stores';
-  import { urlToState } from '$lib/utils/url';
-
   import ImpactTime from "$src/routes/(default)/explore/impacts/ImpactTime/ImpactTime.svelte";
-
-  $: urlToState($page.url);
 </script>
 
 <ImpactTime />
