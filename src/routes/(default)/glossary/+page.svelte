b150b014711de11b9162b7ac679f98e9ac0212a6
jnsprnw <6gqrbj@posteo.org>
2023-04-15 17:36:41 +0200

    Clean up

--- src/routes/(default)/glossary/+page.svelte
+++ src/routes/(default)/glossary/+page.svelte
@@ -1,22 +1,15 @@
 <script>
-  import { groups } from 'd3-array';
-  import { keyBy } from 'lodash-es';
+
   import SectionIntro from '../methodology/SectionIntro.svelte';
   import TermSection from './TermSection.svelte';
   import ScrollContent from '$lib/helper/ScrollContent/ScrollContent.svelte';
   import NestedNav from '$lib/helper/ScrollContent/NestedNav.svelte';
-
   export let data;
-  $: ({ entries, categories, content } = data);
 
-  $: entriesByCategory = groups(entries, (d) => d.category);
-  $: categoriesBySlug = keyBy(categories, 'slug');
-
-  $: sections = content.map((block) => {
+  $: sections = data.content.map((block) => {
     return {
       ...block,
       component: SectionIntro,
-      content: '',
       sections: block.sections.map(section => {
         return {
           ...section,
