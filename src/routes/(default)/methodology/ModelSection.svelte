71aff64c71290509cfceb13ffa7b6be8aaf25745
jnsprnw <6gqrbj@posteo.org>
2023-04-15 16:48:16 +0200

    Better scroll positions
    #127

--- src/routes/(default)/methodology/ModelSection.svelte
+++ src/routes/(default)/methodology/ModelSection.svelte
@@ -8,7 +8,7 @@
   const { query } = getContext('scrollContent');
 </script>
 
-<h3 id={slug} name={slug} class="font-bold text-xl mb-2 mt-4 first:mt-0 {query}">{title}</h3>
+<h3 id={slug} name={slug} class="scroll-mt-4 font-bold text-xl mb-2 mt-4 first:mt-0 {query}">{title}</h3>
 <HtmlContent {content} />
 {#if link}
   <a href={link} class="link">More information about {title}</a>
--- src/routes/(default)/methodology/SectionIntro.svelte
+++ src/routes/(default)/methodology/SectionIntro.svelte
@@ -7,5 +7,5 @@
   const { query } = getContext('scrollContent');
 </script>
 
-<h2 id={slug} class="font-bold text-2xl mb-2 mt-4 first-of-type:mt-0 {query}">{title}</h2>
+<h2 id={slug} name={slug} class="scroll-mt-4 font-bold text-2xl mb-2 mt-4 first-of-type:mt-0 {query}">{title}</h2>
 <HtmlContent {content} />
