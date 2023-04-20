c4915b5b86b589f1507a52f7139ded611543566f
jnsprnw <6gqrbj@posteo.org>
2023-04-20 18:06:37 +0200

    More Netlify Tests. Yay!

--- src/routes/(default)/explore/impacts/ImpactTime/ChartTooltips.svelte
+++ src/routes/(default)/explore/impacts/ImpactTime/ChartTooltips.svelte
@@ -1,7 +1,7 @@
 <script>
-  import tooltipTemplate from './tooltip-template.html?raw';
+  // import tooltipTemplate from './tooltip-template.html?raw';
   import TriggerPoints from '$lib/charts/tooltip/TriggerPoints.svelte';
-  import renderTemplate from '$utils/renderTemplate';
+  // import renderTemplate from '$utils/renderTemplate';
 
   export let data;
 </script>
