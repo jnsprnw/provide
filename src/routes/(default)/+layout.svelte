d95424ec853bfa36acbb29f74a0f82b301e25ab8
jnsprnw <6gqrbj@posteo.org>
2023-04-20 11:42:15 +0200

    Clean up

--- src/routes/(default)/+layout.svelte
+++ src/routes/(default)/+layout.svelte
@@ -4,7 +4,6 @@
   import Header from '$lib/site/Header.svelte';
   import HtmlHead from '$lib/site/HtmlHead.svelte';
   import Footer from '$lib/site/Footer.svelte';
-  import { page } from '$app/stores';
   import _ from 'lodash-es';
 
   _.templateSettings = {
