8697ed82ab885c632b1b93129e4ed200b5244426
jnsprnw <6gqrbj@posteo.org>
2023-04-28 12:17:39 +0200

    Responsiveness
    #52

--- src/lib/site/Header.svelte
+++ src/lib/site/Header.svelte
@@ -12,13 +12,13 @@
 </script>
 
 <nav class="border-b border-foreground-weakest py-7">
-  <div class="mx-auto max-w-7xl px-6 flex justify-between">
+  <div class="mx-auto max-w-7xl px-6 flex justify-between gap-y-4 flex-col md:flex-row">
     <NavLink href="/"><Logo /></NavLink>
-    <ul class="flex gap-x-5">
+    <ul class="flex flex-wrap gap-x-3 md:gap-x-4 lg:gap-x-5">
       {#each items as { label, href }}
         <NavLink
           activeClass="text-theme-base font-bold"
-          class="text-foreground-weak"
+          class="text-foreground-weak text-sm md:text-base"
           {href}>{label}</NavLink
         >
       {/each}
