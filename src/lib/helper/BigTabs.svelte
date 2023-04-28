9f223bba37188c6e55e4eb6574ba84aae5aba6bf
jnsprnw <6gqrbj@posteo.org>
2023-04-28 13:28:15 +0200

    Repsonsiveness
    #52

--- src/lib/helper/BigTabs.svelte
+++ src/lib/helper/BigTabs.svelte
@@ -3,7 +3,7 @@
   export let tabs = [];
 </script>
 
-<ul class="flex gap-10">
+<ul class="flex gap-4 sm:gap-10">
   {#each tabs as { href, label, description, disabled }}
     <li class="">
       <NavLink
@@ -11,19 +11,19 @@
         loc="location"
         let:isActive
         {disabled}
-        class="flex flex-col min-w-[200px] max-w-min pb-3"
+        class="flex flex-col sm:min-w-[200px] sm:max-w-min pb-3"
         activeClass="border-b-4 border-theme-base"
       >
         <span
           class:text-theme-base={isActive}
           class:font-bold={isActive}
-          class="text-3xl mb-1 whitespace-nowrap"
+          class="text-2xl sm:text-3xl mb-1 whitespace-nowrap"
         >
           {label}
         </span>
         <span
           class:text-theme-weaker={isActive}
-          class="w-full text-foreground-weak">{description}</span
+          class="w-full text-foreground-weak text-sm sm:text-base">{description}</span
         >
       </NavLink>
     </li>
