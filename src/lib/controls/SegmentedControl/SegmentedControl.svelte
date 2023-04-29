2a44d653dbd07cafb721c7bf8570177fe1d2a656
Flavio <flavio.gortana@posteo.net>
2023-04-29 12:17:19 +0200

    hiding segmented control in static mode

--- src/lib/controls/SegmentedControl/SegmentedControl.svelte
+++ src/lib/controls/SegmentedControl/SegmentedControl.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <script>
-  import { createEventDispatcher } from 'svelte';
+  import { IS_STATIC } from '$stores/state';
   export let options = [];
   export let value;
   export let label;
@@ -14,26 +14,28 @@
   instance++;
 </script>
 
-<div class="flex">
-  {#if label}
-    <legend class="control-label" for={selectId}>{label}</legend>
-  {/if}
-  <fieldset role="radiogroup" class="flex" id={selectId}>
-    {#each options as option}
-      <div
-        class:text-theme-base={option.uid === value}
-        class:font-bold={option.uid === value}
-        class="cursor-pointer border px-3 py-1 border-foreground-weakest first:border-r-0 flex items-center first:rounded-l last:rounded-r"
-      >
-        <input
-          type="radio"
-          class="appearance-none"
-          id={option.uid}
-          value={option.uid}
-          bind:group={value}
-        />
-        <label class="cursor-pointer" for={option.uid}>{option.label}</label>
-      </div>
-    {/each}
-  </fieldset>
-</div>
+{#if !$IS_STATIC}
+  <div class="flex">
+    {#if label}
+      <legend class="control-label" for={selectId}>{label}</legend>
+    {/if}
+    <fieldset role="radiogroup" class="flex" id={selectId}>
+      {#each options as option}
+        <div
+          class:text-theme-base={option.uid === value}
+          class:font-bold={option.uid === value}
+          class="cursor-pointer border px-3 py-1 border-foreground-weakest first:border-r-0 flex items-center first:rounded-l last:rounded-r"
+        >
+          <input
+            type="radio"
+            class="appearance-none"
+            id={option.uid}
+            value={option.uid}
+            bind:group={value}
+          />
+          <label class="cursor-pointer" for={option.uid}>{option.label}</label>
+        </div>
+      {/each}
+    </fieldset>
+  </div>
+{/if}
--- src/routes/(default)/documentation/ModelSection.svelte
+++ src/routes/(default)/documentation/ModelSection.svelte
@@ -1,6 +1,7 @@
 <script>
   import HtmlContent from '$lib/helper/HtmlContent.svelte';
   import { getContext } from 'svelte';
+
   export let content;
   export let slug;
   export let title;
@@ -8,8 +9,18 @@
   const { query } = getContext('scrollContent');
 </script>
 
-<h3 id={slug} name={slug} class="scroll-mt-4 font-bold text-xl mb-2 mt-4 first:mt-0 {query}">{title}</h3>
+<h3
+  id={slug}
+  name={slug}
+  class="scroll-mt-4 font-bold text-xl mb-2 mt-4 first:mt-0 {query}"
+>
+  {title}
+</h3>
 <HtmlContent {content} />
 {#if link}
-  <a href={link} class="underline decoration-theme-weaker hover:decoration-theme-stronger mt-3 text-sm inline-block">More information about {title}</a>
+  <a
+    href={link}
+    class="underline decoration-theme-weaker hover:decoration-theme-stronger mt-3 text-sm inline-block"
+    >More information about {title}</a
+  >
 {/if}
