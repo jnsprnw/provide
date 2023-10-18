d0e54cfaf50cafd7feef41a62bbf021dcc31fbd9
Jonas Parnow <6gqrbj@posteo.org>
2023-10-18 12:41:04 +0200

    Display all geography types, disabled message

--- src/routes/(default)/explore/MainControls/GeographySelection/GeographySelection.svelte
+++ src/routes/(default)/explore/MainControls/GeographySelection/GeographySelection.svelte
@@ -4,6 +4,7 @@
   import { END_GEO_SHAPE } from '$src/config.js';
   import { writable } from 'svelte/store';
   import { fetchData } from '$lib/api/api';
+  import { sortBy } from 'lodash-es';
   import { GEOGRAPHY_TYPES } from '$stores/meta.js';
   import PopoverSelect from '$lib/controls/PopoverSelect/PopoverSelect.svelte';
   import Content from '$lib/controls/PopoverSelect/Content.svelte';
@@ -14,7 +15,10 @@
 
   $: buttonLabel = $IS_COMBINATION_AVAILABLE_GEOGRAPHY ? $CURRENT_GEOGRAPHY?.label : `Unavailable geography selected`;
 
-  $: geographyTypes = $GEOGRAPHY_TYPES.filter((d) => $AVAILABLE_GEOGOGRAPHIES.filter(({ geographyType }) => geographyType === d.uid).length);
+  $: geographyTypes = sortBy(
+    $GEOGRAPHY_TYPES.map((t) => ({ ...t, disabled: !t.availableIndicators.length })), // The disabled attribute is used to disable the option
+    (t) => !Boolean(t.availableIndicators) // This sorts the available types first
+  );
 
   let hoveredItem;
   let currentFilterUid;
@@ -43,12 +47,39 @@
   buttonClass="border-theme-base/20 border aria-expanded:border-theme-base/60"
   hasWarning={!$IS_COMBINATION_AVAILABLE_GEOGRAPHY}
 >
-  <Content filters={geographyTypes} filterKey="geographyType" filterLabel="Pick a location" currentUid={$CURRENT_GEOGRAPHY_UID} items={$AVAILABLE_GEOGOGRAPHIES} bind:currentFilterUid>
-    <div slot="items" let:items let:currentFilterUid class="grid grid-cols-1 md:grid-cols-[1.5fr_3fr] lg:grid-cols-[1fr_3fr]">
-      <Geographies {items} bind:hoveredItem geographyType={geographyTypes.find((g) => g.uid === currentFilterUid)} bind:currentUid={$CURRENT_GEOGRAPHY_UID} />
+  <Content
+    filters={geographyTypes}
+    filterKey="geographyType"
+    filterLabel="Pick a location"
+    disabledMessage="Geography type not yet available"
+    currentUid={$CURRENT_GEOGRAPHY_UID}
+    items={$AVAILABLE_GEOGOGRAPHIES}
+    bind:currentFilterUid
+  >
+    <div
+      slot="items"
+      let:items
+      let:currentFilterUid
+      class="grid grid-cols-1 md:grid-cols-[1.5fr_3fr] lg:grid-cols-[1fr_3fr]"
+    >
+      <Geographies
+        {items}
+        bind:hoveredItem
+        geographyType={geographyTypes.find((g) => g.uid === currentFilterUid)}
+        bind:currentUid={$CURRENT_GEOGRAPHY_UID}
+      />
       <div class="px-3 hidden md:block">
-        <LoadingWrapper let:asyncProps={{ geoShape }} asyncProps={{ geoShape: $GEO_SHAPE_DATA }} let:isLoading>
-          <Map hovered={hoveredItem} baseLayer={geoShape[0].data.data} dataLayer={geoShape[1].data.data} selected={$CURRENT_GEOGRAPHY_UID} />
+        <LoadingWrapper
+          let:asyncProps={{ geoShape }}
+          asyncProps={{ geoShape: $GEO_SHAPE_DATA }}
+          let:isLoading
+        >
+          <Map
+            hovered={hoveredItem}
+            baseLayer={geoShape[0].data.data}
+            dataLayer={geoShape[1].data.data}
+            selected={$CURRENT_GEOGRAPHY_UID}
+          />
         </LoadingWrapper>
       </div>
     </div>
