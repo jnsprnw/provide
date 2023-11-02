75df38aafb168d294531238298320a8458955477
Jonas Parnow <6gqrbj@posteo.org>
2023-11-02 11:41:32 +0100

    Link fix
    #210
    #206

--- src/routes/(default)/explainers/Scenarios/CrossLink.svelte
+++ src/routes/(default)/explainers/Scenarios/CrossLink.svelte
@@ -1,13 +1,16 @@
 <script>
   import { MAX_NUMBER_SELECTABLE_SCENARIOS } from '$config';
-  import { buildURL } from '$utils/url.js';
-  export let selectedScenarios = [];
   import Arrow from '$lib/helper/icons/Arrow.svelte';
+  import { buildURL } from '$utils/url.js';
+  import { page } from '$app/stores';
+
+  export let selectedScenarios = [];
+
   $: noScenarioSelected = !selectedScenarios.length;
   $: tooManyScenariosSelected = selectedScenarios.length > MAX_NUMBER_SELECTABLE_SCENARIOS;
   $: isDisabled = noScenarioSelected || tooManyScenariosSelected;
 
-  $: url = buildURL('impacts', { scenarios: selectedScenarios.sort().slice(0, MAX_NUMBER_SELECTABLE_SCENARIOS) });
+  $: query = buildURL('impacts', { scenarios: selectedScenarios.sort().slice(0, MAX_NUMBER_SELECTABLE_SCENARIOS) });
 </script>
 
 <!-- We keep this because we remove href (https://www.scottohara.me/blog/2021/05/28/disabled-links.html) -->
@@ -16,8 +19,8 @@
   aria-disabled={String(isDisabled)}
   class:text-theme-weaker={isDisabled}
   class:pointer-events-none={isDisabled}
-  class="bg-surface-weaker hover:bg-surface-weakest transition-color flex gap-x-3 justify-center items-center text-theme-base px-6 py-5 text-center rounded-sm inline"
-  href={isDisabled ? undefined : url}
+  class="bg-surface-weaker col-start-2 hover:bg-surface-weakest transition-color flex gap-x-3 justify-center items-center text-theme-base px-6 py-5 text-center rounded-sm inline"
+  href={isDisabled ? undefined : `${$page.url.origin}/explore/impacts${query}`}
 >
   {#if noScenarioSelected}
     Select scenarios to view them in the explorer
