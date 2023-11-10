46b1277240c1a4f81f3b9fa326454b8ba851a1c5
Jonas Parnow <6gqrbj@posteo.org>
2023-11-10 23:33:17 +0100

    Group reference text

--- src/routes/(default)/explore/avoid/Reference/Text.svelte
+++ src/routes/(default)/explore/avoid/Reference/Text.svelte
@@ -10,10 +10,11 @@
   $: ({ reference } = $CURRENT_INDICATOR_OPTIONS);
 </script>
 
-<strong>What level of impact should be avoided?</strong>
-
-<p class="text-text-weaker text-sm mr-2">
-  Over the {reference.label} period, in {$CURRENT_GEOGRAPHY.label} the {labelWithinSentence} was on average <strong>{formatValue(average_value, unit.uid)}</strong>, and reached a maximum of
-  <strong>{formatValue(max.value, unit.uid)}</strong>
-  in {max.year}.
-</p>
+<div>
+  <strong>What level of impact should be avoided?</strong>
+  <p class="text-text-weaker text-sm mr-2">
+    Over the {reference.label} period, in {$CURRENT_GEOGRAPHY.label} the {labelWithinSentence} was on average <strong>{formatValue(average_value, unit.uid)}</strong>, and reached a maximum of
+    <strong>{formatValue(max.value, unit.uid)}</strong>
+    in {max.year}.
+  </p>
+</div>
