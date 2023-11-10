9a13f715487198fb468e85566bc93c553be22add
Jonas Parnow <6gqrbj@posteo.org>
2023-11-10 16:13:44 +0100

    Table styling, scenario info
    #204
    #206

--- src/routes/(default)/explainers/Scenarios/Table/Table.svelte
+++ src/routes/(default)/explainers/Scenarios/Table/Table.svelte
@@ -1,8 +1,9 @@
 <script>
   import { KEY_CHARACTERISTICS } from '$config';
   import chroma from 'chroma-js';
-  import { extent, max, sum } from 'd3-array';
+  import { extent, max } from 'd3-array';
   import { formatValue } from '$lib/utils/formatting';
+  import tooltip from '$lib/utils/tooltip';
   import THEME from '$styles/theme-store.js';
 
   export let scenariosListed = [];
@@ -53,7 +54,7 @@
     const scale = chroma
       .scale([parseTailwindColor($THEME.color.surface.weakest), parseTailwindColor($THEME.color.theme.base)])
       .domain(domain)
-      .mode('lch'); // TODO: Change colors.
+      .mode('lch');
     return {
       key,
       scale,
@@ -64,7 +65,7 @@
   });
 
   $: scenarios = scenariosListed.map((scenario, i) => {
-    const { uid, label } = scenario;
+    const { uid, label, description } = scenario;
     const isSelected = selectedScenarios.includes(uid);
     const scenarioSelectedIndex = selectedScenarios.indexOf(uid);
     const hasBorderBottom = i !== scenariosListed.length - 1;
@@ -88,6 +89,7 @@
       borderColorLeft,
       hasBorderBottom,
       values,
+      description,
     };
   });
 
@@ -98,10 +100,13 @@
     })
     .join(' ');
 
+  // Width of the whole table
   let widthTable = 0;
-  let heightColumns = [];
+  // With of the columns without the static part
   let widthColumns = 0;
+  // Ref to the table
   let table;
+  // Scroll position to the left
   let sleft = 0;
 </script>
 
@@ -143,14 +148,13 @@
       role="rowgroup"
       class="grid max-w-full relative"
     >
-      {#each scenarios as { i, uid, label, values, borderColorLeft }}
+      {#each scenarios as { i, uid, label, values, borderColorLeft, description }}
         <button
           role="row"
           aria-rowindex={i}
           class:border-b={i !== scenariosListed.length - 1}
           class="max-w-full border-b-contour-weakest text-white focus:bg-surface-weaker focus:text-surface-weaker hover:bg-surface-weaker hover:text-surface-weaker"
           bind:clientWidth={widthColumns}
-          bind:clientHeight={heightColumns[i]}
         >
           <label
             for={uid}
@@ -171,6 +175,28 @@
                 bind:group={selectedScenarios}
               />
               <span class="text-sm font-bold inline-block text-gray-800 overflow-hidden text-ellipsis">{label}</span>
+              <svg
+                use:tooltip={{ content: description }}
+                xmlns="http://www.w3.org/2000/svg"
+                class="text-contour-weaker/60 w-3.5 h-3.5"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path
+                  stroke="none"
+                  d="M0 0h24v24H0z"
+                  fill="none"
+                ></path>
+                <path
+                  d="M12 2c5.523 0 10 4.477 10 10a10 10 0 0 1 -19.995 .324l-.005 -.324l.004 -.28c.148 -5.393 4.566 -9.72 9.996 -9.72zm0 9h-1l-.117 .007a1 1 0 0 0 0 1.986l.117 .007v3l.007 .117a1 1 0 0 0 .876 .876l.117 .007h1l.117 -.007a1 1 0 0 0 .876 -.876l.007 -.117l-.007 -.117a1 1 0 0 0 -.764 -.857l-.112 -.02l-.117 -.006v-3l-.007 -.117a1 1 0 0 0 -.876 -.876l-.117 -.007zm.01 -3l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007z"
+                  stroke-width="0"
+                  fill="currentColor"
+                ></path>
+              </svg>
             </div>
             {#each values as { label, bg, useBlackFont }}
               <span
