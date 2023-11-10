b9589b089ed2890b83b9e17ebf3cec9bedfabc38
Jonas Parnow <6gqrbj@posteo.org>
2023-11-10 22:44:26 +0100

    Share warning message

--- src/lib/helper/FallbackMessage.svelte
+++ src/lib/helper/FallbackMessage.svelte
@@ -1,10 +1,7 @@
 <script>
-  import { DEFAULT_GEOGRAPHY_UID, DEFAULT_INDICATOR_UID, DEFAULT_SCENARIOS_UID } from '$src/config';
+  import Message from '$lib/helper/Message.svelte';
   import {
     IS_EMPTY_INDICATOR,
-    // CURRENT_GEOGRAPHY_UID,
-    // CURRENT_INDICATOR_UID,
-    // CURRENT_SCENARIOS_UID,
     IS_COMBINATION_AVAILABLE_INDICATOR,
     IS_COMBINATION_AVAILABLE_SCENARIO,
     CURRENT_SCENARIOS,
@@ -23,30 +20,20 @@
   const listFormat = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
 </script>
 
-<div
-  class="flex flex-col gap-y-2 items-center justify-center py-24 min-h-[70vh]"
-  role="alert"
->
-  {#if $IS_EMPTY_SELECTION}
-    {#if $IS_EMPTY_INDICATOR}
-      <strong class="text-xl font-bold">No indicator selected</strong>
-      <span class="text-contour-weaker">Select an indicator from the dropdown at the top of this page.</span>
-    {:else if $IS_EMPTY_SCENARIO}
-      <strong class="text-xl font-bold">No scenario selected</strong>
-      <span class="text-contour-weaker">Select one or more scenarios from the dropdown at the left hand side of this page.</span>
-    {/if}
-  {:else if !$IS_COMBINATION_AVAILABLE}
-    <strong class="text-xl font-bold">There is no data for your current selection</strong>
+{#if $IS_EMPTY_SELECTION}
+  {#if $IS_EMPTY_INDICATOR}
+    <Message headline="No indicator selected">
+      <span>Select an indicator from the dropdown at the top of this page.</span>
+    </Message>
+  {:else if $IS_EMPTY_SCENARIO}
+    <Message headline="No scenario selected">
+      <span>Select one or more scenarios from the dropdown at the left hand side of this page.</span>
+    </Message>
+  {/if}
+{:else if !$IS_COMBINATION_AVAILABLE}
+  <Message headline="There is no data for your current selection">
     {#if unavailableItems.length}
       <span class="text-contour-weaker">The selected {listFormat.format(unavailableItems)} {unavailableItems.length > 1 ? 'are' : 'is'} not available in this combination.</span>
     {/if}
-  {/if}
-  <!-- <button
-    class="text-theme-base mt-4"
-    on:click={() => {
-      $CURRENT_INDICATOR_UID = DEFAULT_INDICATOR_UID;
-      $CURRENT_GEOGRAPHY_UID = DEFAULT_GEOGRAPHY_UID;
-      $CURRENT_SCENARIOS_UID = DEFAULT_SCENARIOS_UID;
-    }}>Reset</button
-  >-->
-</div>
+  </Message>
+{/if}
---
+++ src/lib/helper/Message.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let warningBackground = true;
+  export let warningSizeSmall = false;
+  export let warningSign = false;
+  export let headline;
+</script>
+
+<div
+  class="text-center py-36 px-2 flex flex-col gap-y-2 border-surface-weakest rounded-sm"
+  class:bg-surface-weaker={warningBackground}
+  class:border={warningBackground}
+  class:py-8={warningSizeSmall}
+  role="alert"
+>
+  <span
+    class="text-lg font-bold leading-tight"
+    class:text-base={warningSizeSmall}
+    >{#if warningSign}⚠️
+    {/if}{headline}</span
+  >
+  <div
+    class="flex-col text-sm"
+    class:flex={!warningSizeSmall}
+    class:text-xs={warningSizeSmall}
+  >
+    <slot />
+  </div>
+</div>
