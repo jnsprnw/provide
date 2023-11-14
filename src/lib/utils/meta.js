f4ac8c3e53cf7b619ad75454c488ca6e21c82181
Jonas Parnow <6gqrbj@posteo.org>
2023-11-14 17:38:34 +0100

    Fix if no scenario is selected

--- src/lib/utils/meta.js
+++ src/lib/utils/meta.js
@@ -8,7 +8,7 @@ export function extractTimeframe(scenario) {
   // TODO: This will not be needed when the API provides the correct data
   if (scenario.hasOwnProperty(KEY_SCENARIO_TIMEFRAME)) {
     return scenario[KEY_SCENARIO_TIMEFRAME];
-  } else if (scenario[KEY_CHARACTERISTICS].hasOwnProperty(KEY_SCENARIO_TIMEFRAME)) {
+  } else if (scenario.hasOwnProperty(KEY_CHARACTERISTICS) && scenario[KEY_CHARACTERISTICS].hasOwnProperty(KEY_SCENARIO_TIMEFRAME)) {
     console.warn(`${KEY_SCENARIO_TIMEFRAME} of the scenario should be placed at the top level.`);
     return scenario[KEY_CHARACTERISTICS][KEY_SCENARIO_TIMEFRAME];
   } else if (Array.isArray(scenario.timeframe) && scenario.timeframe.length == 2) {
