e8cf5bd12f66c32bda3efa6da4f6625b164554cd
Jonas Parnow <6gqrbj@posteo.org>
2023-11-15 19:13:15 +0100

    Use scroll indicators for presets
    #206

--- src/routes/(default)/explainers/Scenarios/Presets.svelte
+++ src/routes/(default)/explainers/Scenarios/Presets.svelte
@@ -4,6 +4,7 @@
   import { writable } from 'svelte/store';
   import { KEY_SCENARIOPRESET_UID as PRESET_ID } from '$config';
   import Tagline from '$lib/helper/Tagline.svelte';
+  import SideScrollIndicator from '$lib/helper/SideScrollIndicator.svelte';
 
   const dispatch = createEventDispatcher();
 
@@ -39,30 +40,46 @@
       }
     }
   }
+  // Width of the content
+  let widthContent = 0;
 </script>
 
 {#if scenarioPresetsForCurrentTimeframe.length}
   <div>
     <Tagline class="mb-2">Scenario Presets</Tagline>
-    <RadioGroup
-      bind:value={$currentPreset}
-      class="grid gap-x-2.5"
-      style="grid-template-columns: repeat({scenarioPresetsForCurrentTimeframe.length}, minmax(0, 1fr));"
-    >
-      {#each scenarioPresetsForCurrentTimeframe as { uid: value, description, title }}
-        <RadioGroupOption
-          {value}
-          let:checked
+    <RadioGroup bind:value={$currentPreset}>
+      <SideScrollIndicator
+        widthOfContent={widthContent}
+        distanceLeft={0}
+        distanceRight={0}
+      >
+        <div
+          class="grid gap-x-2.5 min-w-min grid-rows-[auto_1fr]"
+          style="grid-template-columns: repeat({scenarioPresetsForCurrentTimeframe.length * 2}, 1fr);"
+          bind:clientWidth={widthContent}
         >
-          <div
-            class:bg-surface-weaker={checked}
-            class="grid hover:bg-surface-weaker/50 grid-rows-[auto_1fr] h-full gap-y-1 px-2.5 py-2"
-          >
-            <span class="text-sm text-theme-base font-bold">{title}</span>
-            <span class="text-xs">{description}</span>
-          </div>
-        </RadioGroupOption>
-      {/each}
+          {#each [...scenarioPresetsForCurrentTimeframe, ...scenarioPresetsForCurrentTimeframe] as { uid: value, description, title }}
+            <RadioGroupOption
+              {value}
+              let:checked
+              class=" grid  h-full gap-y-1 "
+              style="grid-template-rows: subgrid; grid-row: span 2;"
+            >
+              <div
+                aria-selected={checked}
+                class="grid min-w-[250px] aria-selected:bg-theme-weakest hover:bg-surface-weaker/50 px-2.5 py-2 rounded-sm"
+                style="grid-template-rows: subgrid; grid-row: span 2;"
+              >
+                <span
+                  class="text-sm text-theme-base font-bold text-theme"
+                  class:text-theme-stronger={checked}>{title}</span
+                >
+                <span class="text-xs">{description}</span>
+              </div>
+            </RadioGroupOption>
+          {/each}
+        </div>
+      </SideScrollIndicator>
     </RadioGroup>
   </div>
 {/if}
