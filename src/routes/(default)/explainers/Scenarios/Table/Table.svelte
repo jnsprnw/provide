0fdf18c208e5c55de2ff15bd19d112c5da4b513a
Jonas Parnow <6gqrbj@posteo.org>
2023-11-18 00:50:37 +0100

    Custom order, styling, correct titles
     #204

--- src/routes/(default)/explainers/Scenarios/Table/Table.svelte
+++ src/routes/(default)/explainers/Scenarios/Table/Table.svelte
@@ -7,6 +7,8 @@
   import Info from '$lib/helper/icons/Info.svelte';
   import SideScrollIndicator from '$lib/helper/SideScrollIndicator.svelte';
   import THEME from '$styles/theme-store.js';
+  import { orderBy } from 'lodash-es';
+  import { writable } from 'svelte/store';
 
   export let scenariosListed = [];
   export let selectedScenarios = [];
@@ -28,31 +30,31 @@
   // The output of the function is used for the coloring. The fallback is a simple return of the value
   const COLUMNS = {
     2100: [
-      ['Peak GMT', 'gmtPeak', ([value, year]) => `${formatValue(value, 'degrees-celsius')} in ${year}`, ([v]) => v + v * Math.random()],
-      ['2100 GMT', 'gmt2100', (value) => formatValue(value, 'degrees-celsius'), (v) => v + v * Math.random()],
-      ['Cooling rate after peak', 'coolingRateAfterPeak', (value) => `${formatValue(value, 'degrees-celsius')} / decade`, (v) => v + v * Math.random()],
-      ['2050 emissions', 'emissions2050', (value) => `${formatValue(value, 'integer')} GtCO₂`, (v) => v + v * Math.random()],
-      ['2100 emissions', 'emissions2100', (value) => `${formatValue(value, 'integer')} GtCO₂`, (v) => v + v * Math.random()],
+      ['Peak GMT', 'gmtPeak', ([value, year]) => `${formatValue(value, 'degrees-celsius')} in ${year}`, ([value]) => value],
+      ['2100 GMT', 'gmt2100', (value) => formatValue(value, 'degrees-celsius')],
+      ['Cooling rate after peak', 'coolingRateAfterPeak', (value) => `${formatValue(value, 'degrees-celsius')} / decade`],
+      ['2050 emissions', 'emissions2050', (value) => `${formatValue(value, 'integer')} GtCO₂`],
+      ['2100 emissions', 'emissions2100', (value) => `${formatValue(value, 'integer')} GtCO₂`],
       ['Timing of NZ CO₂', 'timingNZCO2', (value) => value, (v) => v + v * Math.random()],
-      ['Timing of NZ GHG', 'timingNZGHG', (value) => value, (v) => v + v * Math.random()],
+      ['Timing of NZ GHG', 'timingNZGHG', (value) => value],
       ['Likelihood PW < 1.5°C', 'likelihood15', (value) => formatValue(value, 'percent'), (v) => v + v * Math.random()],
       ['Likelihood PW < 2°C', 'likelihood2', (value) => formatValue(value, 'percent'), (v) => v + v * Math.random()],
       ['Likelihood PW < 3°C', 'likelihood3', (value) => formatValue(value, 'percent'), (v) => v + v * Math.random()],
     ],
     2300: [
-      ['Peak GMT', 'gmtPeak', ([value, year]) => `${formatValue(value, 'degrees-celsius')} in ${year}`, ([v]) => v + v * Math.random()],
-      ['2100 GMT', 'gmt2100', (value) => formatValue(value, 'degrees-celsius'), (v) => v + v * Math.random()],
-      ['2300 GMT', 'gmt2300', (value) => formatValue(value, 'degrees-celsius'), (v) => v + v * Math.random()],
-      ['Cooling rate after peak', 'coolingAfterPeak', (value) => `${formatValue(value, 'degrees-celsius')} / decade`, (v) => v + v * Math.random()],
+      ['Peak GMT', 'gmtPeak', ([value, year]) => `${formatValue(value, 'degrees-celsius')} in ${year}`, ([value]) => value],
+      ['2100 GMT', 'gmt2100', (value) => formatValue(value, 'degrees-celsius')],
+      ['2300 GMT', 'gmt2300', (value) => formatValue(value, 'degrees-celsius')],
+      ['Cooling after peak', 'coolingAfterPeak', (value) => `${formatValue(value, 'degrees-celsius')}`],
       ['Timing of NZ CO2', 'timingNZCO2', (value) => value, (v) => v + v * Math.random()],
-      ['Timing of NZ GHG', 'timingNZGHG', (value) => value, (v) => v + v * Math.random()],
+      ['Timing of NZ GHG', 'timingNZGHG', (value) => value],
     ],
   };
 
   $: titleWidth =
     {
       2100: 250,
-      2300: 300,
+      2300: 350,
     }[selectedTimeframe] ?? 250;
 
   // Get the values for each key and create color scales for each.
@@ -72,6 +74,8 @@
     };
   });
 
+  $: console.log(tableColumns.map(({ get }) => get));
+
   $: scenarios = scenariosListed.map((scenario, i) => {
     const { uid, label, description, isPrimary } = scenario;
     const isSelected = selectedScenarios.includes(uid);
@@ -90,6 +94,12 @@
         useBlackFont,
       };
     });
+    const order = Object.fromEntries(
+      tableColumns.map(({ key, get }) => {
+        const value = get(scenario[KEY_CHARACTERISTICS][key]);
+        return [key, value];
+      })
+    );
     return {
       i,
       uid,
@@ -99,8 +109,14 @@
       isPrimary,
       values,
       description,
+      ...order,
     };
   });
+
+  const sorting = writable('i');
+  const sortingDirection = writable(1);
+
+  $: sortedScenarios = orderBy(scenarios, (s) => s[$sorting], $sortingDirection > 0 ? 'asc' : 'desc');
 
   $: maxWidth = tableColumns
     .map((_, i) => {
@@ -135,14 +151,21 @@
           role="gridcell"
           class="sticky left-0 bg-white px-4 text-xs border-b-contour-weakest border-b flex items-end py-3"
         >
-          Scenario
+          <button
+            on:click={$sorting === 'i' ? sortingDirection.update((v) => v * -1) : sorting.set('i')}
+            class:font-bold={$sorting === 'i'}>Scenario</button
+          >
         </div>
-        {#each tableColumns as { label }}
+        {#each tableColumns as { label, key }}
+          {@const isActive = $sorting === key}
           <div
             role="gridcell"
             class="text-xs px-1 border-b-contour-weakest border-b flex items-end justify-center text-center leading-tight py-3"
           >
-            {label}
+            <button
+              on:click={isActive ? sortingDirection.update((v) => v * -1) : sorting.set(key)}
+              class:font-bold={isActive}>{label}</button
+            >
           </div>
         {/each}
       </div>
@@ -151,11 +174,11 @@
       role="rowgroup"
       class="grid max-w-full relative"
     >
-      {#each scenarios as { i, uid, label, values, borderColorLeft, description, isPrimary }}
+      {#each sortedScenarios as { i, uid, label, values, borderColorLeft, description, isPrimary }, index}
         <button
           role="row"
           aria-rowindex={i}
-          class:border-b={i !== scenariosListed.length - 1}
+          class:border-b={index !== scenariosListed.length - 1}
           class="max-w-full border-b-contour-weakest text-white focus:bg-surface-weaker focus:text-surface-weaker hover:bg-surface-weaker hover:text-surface-weaker"
           bind:clientWidth={widthColumns}
         >
@@ -190,7 +213,7 @@
             {#each values as { label, bg, useBlackFont }}
               <span
                 role="gridcell"
-                class="py-3 flex items-center justify-end px-3 text-xs"
+                class="py-3 flex items-center justify-end px-3 text-xs whitespace-nowrap"
                 style="background-color: {bg}; color: {useBlackFont ? '#000' : '#fff'};">{label ?? '—'}</span
               >
             {/each}
