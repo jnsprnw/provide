3fa7fc4ff931fd5f2c1ae479627cdd96c6e28c2b
Jonas Parnow <6gqrbj@posteo.org>
2023-12-01 18:30:32 +0100

    Disable button for invalid selections
    #223

--- src/routes/(default)/impacts/ShareLink/ShareLink.svelte
+++ src/routes/(default)/impacts/ShareLink/ShareLink.svelte
@@ -1,22 +1,34 @@
 <script>
-  import { CURRENT_GEOGRAPHY_UID, CURRENT_INDICATOR_UID, CURRENT_INDICATOR_OPTION_VALUES, CURRENT_SCENARIOS_UID, CURRENT_INDICATOR_OPTIONS } from '$stores/state.js';
+  import { IS_COMBINATION_AVAILABLE, IS_EMPTY_SELECTION, CURRENT_GEOGRAPHY_UID, CURRENT_INDICATOR_UID, CURRENT_INDICATOR_OPTION_VALUES, CURRENT_SCENARIOS_UID, CURRENT_INDICATOR_OPTIONS } from '$stores/state.js';
   import { buildURL } from '$utils/url.js';
   import tooltip from '$lib/utils/tooltip';
   import copy from 'copy-to-clipboard';
   import { page } from '$app/stores';
 
-  $: query = buildURL('impacts', { indicator: $CURRENT_INDICATOR_UID, geography: $CURRENT_GEOGRAPHY_UID, scenarios: $CURRENT_SCENARIOS_UID, ...$CURRENT_INDICATOR_OPTION_VALUES });
+  $: isDisabled = $IS_EMPTY_SELECTION || !$IS_COMBINATION_AVAILABLE;
+
+  $: query = !isDisabled && buildURL('impacts', { indicator: $CURRENT_INDICATOR_UID, geography: $CURRENT_GEOGRAPHY_UID, scenarios: $CURRENT_SCENARIOS_UID, ...$CURRENT_INDICATOR_OPTION_VALUES });
 
   function handleClick() {
-    const { origin, pathname } = $page.url;
-    copy(`${origin}${pathname}${query}`);
+    if (query) {
+      const { origin, pathname } = $page.url;
+      if (origin && pathname) {
+        copy(`${origin}${pathname}${query}`);
+      } else {
+        console.warn(`Could not determine origin or pathname.`);
+      }
+    } else {
+      console.warn(`Could not determine query.`);
+    }
+
   }
 </script>
 
 <button
-  class="px-3 py-2 hover:bg-white rounded-xl transition-colors"
-  on:click={handleClick}
-  use:tooltip={{ content: 'Click to copy the link to the clipboard' }}
+  aria-disabled={isDisabled}
+  class="text-sm font-bold px-3 py-2 hover:bg-white rounded-xl transition-colors text-theme-base aria-disabled:hover:bg-transparent aria-disabled:cursor-not-allowed aria-disabled:text-theme-weaker"
+  on:click={!isDisabled && handleClick}
+  use:tooltip={{ content: isDisabled ? 'Select a geography and indicator first' : 'Click to copy the link to the clipboard' }}
 >
   Share these results<br />
 </button>
