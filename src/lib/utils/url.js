9f11c7baa99c01ab059a072fb5f9253dcc809ef6
Jonas Parnow <6gqrbj@posteo.org>
2023-12-04 16:56:08 +0100

    Avoid impacts options added
    #223

--- src/lib/utils/url.js
+++ src/lib/utils/url.js
@@ -1,6 +1,7 @@
 import { browser } from '$app/environment';
 import { CURRENT_GEOGRAPHY_UID, CURRENT_INDICATOR_OPTION_VALUES, CURRENT_INDICATOR_UID, CURRENT_SCENARIOS_UID } from '$stores/state.js';
-import { PATH_IMPACT, URL_PATH_INDICATOR, URL_PATH_GEOGRAPHY, URL_PATH_SCENARIOS, URL_PATH_TIME, URL_PATH_REFERENCE, URL_PATH_SPATIAL } from '$config';
+import { SELECTED_LIKELIHOOD_LEVEL, LEVEL_OF_IMPACT_ARRAY } from '$stores/avoid.js';
+import { URL_PATH_FREQUENCY, URL_PATH_INDICATOR_VALUE, URL_PATH_CERTAINTY_LEVEL, URL_PATH_LEVEL_OF_IMPACT, PATH_AVOID, URL_PATH_INDICATOR, URL_PATH_GEOGRAPHY, URL_PATH_SCENARIOS, URL_PATH_TIME, URL_PATH_REFERENCE, URL_PATH_SPATIAL } from '$config';
 import { autoType } from 'd3-dsv';
 import { parse, stringify } from 'qs';
 
@@ -11,10 +12,11 @@ const urlToStateMapping = [
   { key: URL_PATH_GEOGRAPHY, store: CURRENT_GEOGRAPHY_UID },
   { key: URL_PATH_SCENARIOS, store: CURRENT_SCENARIOS_UID },
   {
-    key: [URL_PATH_TIME, URL_PATH_REFERENCE, URL_PATH_SPATIAL],
+    key: [URL_PATH_TIME, URL_PATH_REFERENCE, URL_PATH_FREQUENCY, URL_PATH_SPATIAL, URL_PATH_INDICATOR_VALUE],
     store: CURRENT_INDICATOR_OPTION_VALUES,
-    mode: MODE_MERGE,
   },
+  { key: URL_PATH_CERTAINTY_LEVEL, store: SELECTED_LIKELIHOOD_LEVEL },
+  { key: URL_PATH_LEVEL_OF_IMPACT, store: LEVEL_OF_IMPACT_ARRAY, isIndicatorArray: true },
 ];
 
 export const parseUrlQuery = (url) => {
@@ -38,51 +40,59 @@ function removeParamFromURL(param, key, url) {
   }
 }
 
-function changeStoreToValue(store, value, mode) {
-  if (mode === MODE_MERGE) {
+function changeStoreToValue(store, value, { mode, isIndicatorArray }) {
+  if (mode === MODE_MERGE) { // This is used for the indicator options
     store.update((d) => ({ ...d, ...value }));
   } else {
-    store.set(value);
+    if (isIndicatorArray) { // This is currently only used for level of impact, because MeltUI uses an array for ranges
+      store.set(Array.isArray(value) ? value : [value]);
+    } else { // This is the normal case
+      store.set(value);
+    }
   }
 }
 
 export function urlToState(currentUrl) {
   const url = new URL(currentUrl);
   const params = parse(url.search.replace(/^\?/, ''));
-  urlToStateMapping.forEach(({ store, key, mode }) => {
+  urlToStateMapping.forEach(({ store, key, isIndicatorArray = false }) => {
     let param;
-    if (Array.isArray(key)) {
+    if (Array.isArray(key)) { // This is for indicator options
       const obj = {};
       key.forEach((k) => {
         param = params[k];
-        if (!param) return;
+        if (!param) return; // If setting is not present in the url
         removeParamFromURL(param, k, url);
         obj[k] = param;
       });
-      changeStoreToValue(store, obj, mode);
-    } else {
+      changeStoreToValue(store, obj, { mode: MODE_MERGE });
+    } else { // This is for regular settings
       param = params[key];
-      if (!param) return;
+      if (!param) return; // If setting is not present in the url
+      // TODO: Check for max number of scenarios
       removeParamFromURL(param, key, url);
-      changeStoreToValue(store, param);
+      changeStoreToValue(store, param, { isIndicatorArray });
     }
   });
   if (browser) window.history.replaceState(window.history.state, null, url.href);
 }
 
-export function buildURL(type = PATH_IMPACT, { indicator, geography, scenarios, time, reference, spatial } = {}) {
-  let obj;
-  if (type === PATH_IMPACT) {
-    obj = {
-      [URL_PATH_INDICATOR]: indicator,
-      [URL_PATH_GEOGRAPHY]: geography,
-      [URL_PATH_SCENARIOS]: (scenarios ?? []).sort(),
-      [URL_PATH_TIME]: time,
-      [URL_PATH_REFERENCE]: reference,
-      [URL_PATH_SPATIAL]: spatial,
-    };
+export function buildURL(url, params = {}) {
+  const obj = {
+    [URL_PATH_INDICATOR]: params[URL_PATH_INDICATOR],
+    [URL_PATH_GEOGRAPHY]: params[URL_PATH_GEOGRAPHY],
+    [URL_PATH_SCENARIOS]: (params[URL_PATH_SCENARIOS] ?? []).sort(),
+    [URL_PATH_TIME]: params[URL_PATH_TIME],
+    [URL_PATH_REFERENCE]: params[URL_PATH_REFERENCE],
+    [URL_PATH_SPATIAL]: params[URL_PATH_SPATIAL],
+    [URL_PATH_FREQUENCY]: params[URL_PATH_FREQUENCY],
+    [URL_PATH_INDICATOR_VALUE]: params[URL_PATH_INDICATOR_VALUE],
   }
-  // TODO: Other type
+  if (url === PATH_AVOID) {
+    obj[URL_PATH_LEVEL_OF_IMPACT] = params[URL_PATH_LEVEL_OF_IMPACT];
+    obj[URL_PATH_CERTAINTY_LEVEL] = params[URL_PATH_CERTAINTY_LEVEL];
+  }
+
   const query = stringify(obj, {
     encodeValuesOnly: true,
   });
