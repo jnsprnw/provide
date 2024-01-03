499736304e4c7f4c7aae453dda04708ba541fbb2
Jonas Parnow <6gqrbj@posteo.org>
2024-01-03 13:51:13 +0100

    Clean up

--- src/lib/site/HtmlHead.svelte
+++ src/lib/site/HtmlHead.svelte
@@ -3,23 +3,20 @@
   import { generatePageTitle } from '$utils/meta.js';
   import { IS_STAGING_VERSION } from '$src/config.js';
   const url = 'https://climate-risk-dashboard.climateanalytics.org/';
-  const author =
-    'PROVIDE consortium under direction of Climate Analytics with Jonas Parnow and Flavio Gortana';
-  const keywords =
-    'climate impacts,climate change,climate modelling,climate scenarios,local impacts,temperature overshoot, adaptation, emissions pathways';
-  const description =
-    'Explore future impacts from climate change as the world warms.';
+  const author = 'PROVIDE consortium under direction of Climate Analytics with Jonas Parnow and Flavio Gortana';
+  const keywords = 'climate impacts,climate change,climate modelling,climate scenarios,local impacts,temperature overshoot, adaptation, emissions pathways';
+  const description = 'Explore future impacts from climate change as the world warms.';
   $: title = $page.data.title ?? generatePageTitle();
 </script>
 
 <svelte:head>
-  <title>{ title }</title>
-  <link rel="canonical" href="{url}/" />
+  <title>{title}</title>
+  <link rel="canonical" href={url} />
   <meta name="author" content={author} />
   <meta name="description" content={description} />
   <meta name="news_keywords" content={keywords} />
 
-  {#if IS_STAGING_VERSION}<meta name="robots" content="none, noimageindex, noarchive">{/if}
+  {#if IS_STAGING_VERSION}<meta name="robots" content="none, noimageindex, noarchive" />{/if}
 
   <meta name="robots" content="max-image-preview:large" />
   <meta name="twitter:card" content="summary_large_image" />
