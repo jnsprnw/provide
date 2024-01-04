c81b79f2ec1243648ecb032ab3d47a920a10f65b
Jonas Parnow <6gqrbj@posteo.org>
2024-01-04 18:50:51 +0100

    Fixed size

--- src/routes/(default)/impacts/MainControls/GeographySelection/Geographies.svelte
+++ src/routes/(default)/impacts/MainControls/GeographySelection/Geographies.svelte
@@ -68,41 +68,20 @@
   });
 </script>
 
-<div class="md:border-r border-contour-weakest pb-0 flex flex-col items-stretch">
+<div class="md:border-r border-contour-weakest pb-0 flex flex-col items-stretch" style="min-width: 0;">
   <div class="px-5 grow flex items-stretch my-4">
-    <input
-      type="text"
-      class="px-2 py-2 leading-8 border w-full"
-      bind:value={term}
-      placeholder={`Search for ${geographyTypeLabel}…`}
-    />
+    <input type="search" class="px-2 py-2 leading-8 border w-full" bind:value={term} placeholder={`Search for ${geographyTypeLabel}…`} />
   </div>
-  <RadioGroup
-    bind:value={currentUid}
-    on:change={(e) => (currentUid = e.detail)}
-    class="h-96 w-64 w-full overflow-y-scroll overflow-x-hidden"
-  >
+  <RadioGroup bind:value={currentUid} on:change={(e) => (currentUid = e.detail)} class="h-96 w-full overflow-y-scroll overflow-x-hidden">
     {#key results.length}
       {#if results.length}
         {#each results as { icon, uid, label, emoji }}
-          <RadioGroupOption
-            value={uid}
-            let:checked
-          >
-            <InteractiveListItem
-              icon={icon ?? emoji}
-              {label}
-              {uid}
-              selected={checked}
-              bind:hovered={hoveredItem}
-            />
+          <RadioGroupOption value={uid} let:checked class="focus:bg-surface-weaker focus:outline-none">
+            <InteractiveListItem icon={icon ?? emoji} {label} {uid} selected={checked} bind:hovered={hoveredItem} />
           </RadioGroupOption>
         {/each}
       {:else}
-        <span
-          class="text-xs py-1 px-5 block text-text-weaker"
-          role="status">Could not find any geographies for this type.</span
-        >
+        <span class="text-xs py-1 px-5 block text-text-weaker" role="status">Could not find any geographies for this type.</span>
       {/if}
     {/key}
   </RadioGroup>
