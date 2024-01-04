ff922d847dd55cd759dab50b022badaa03a9c107
Jonas Parnow <6gqrbj@posteo.org>
2024-01-04 15:26:55 +0100

    Globe styling

--- src/routes/(default)/landing-page/Entrypoints.svelte
+++ src/routes/(default)/landing-page/Entrypoints.svelte
@@ -1,56 +1,43 @@
 <script>
-  import { PATH_EXPLORE, PATH_IMPACT } from '$config';
-  import Tagline from '$lib/helper/Tagline.svelte';
+  import { PATH_IMPACT, PATH_AVOID } from '$config';
+  import Entrypoint from './Entrypoint.svelte';
 </script>
 
-<header
-  class="border-b border-b-contour-weaker py-20 justify-center flex-col max-w-5xl mx-auto mb-20 gap-x-12 grid md:grid-cols-2"
->
+<header class="py-20 justify-center flex-col max-w-7xl mx-auto gap-x-12 grid md:grid-cols-2">
   <div>
-    <span class="block font-bold text-2xl mb-3">How to start?</span>
+    <span class="block font-bold text-3xl mb-3">One dashboard to combine opposing perspectives</span>
     <p class="leading-normal text-xl">
-      The <strong>climate risk dashboard</strong> allows you to explore future impacts
-      from climate change as the world warms. Start by picking a mode.
+      The PROVIDE <strong>climate risk dashboard</strong> bundles data from a variety of sources. It includes the latest sciences from our partners at lorem ipsum, dolor, something, something.
     </p>
+    <p class="leading-normal text-xl">It allows you to explore future impacts from climate change as the world warms. Start by picking a mode below.</p>
   </div>
 </header>
-<div class="grid md:grid-cols-2 gap-y-12 gap-x-12 mb-24 max-w-5xl mx-auto">
-  <section class="grid grid-rows-[1fr_auto_auto]">
-    <div class="flex flex-col gap-y-2">
-      <Tagline>Impacts</Tagline>
-      <span class="block text-xl leading-normal font-bold">
-        Pick a geography, select one or several scenarios, and explore
-        corresponding impacts
-      </span>
-    </div>
-    <figure class="bg-surface-weaker px-8 py-16 my-4">
-      <img
-        src="/img/impacts.png"
-        alt="One scenario and many resulting impacts"
-      />
-    </figure>
-    <a
-      class="bg-theme-base text-white p-5 text-center rounded-sm block"
-      href={`/${PATH_EXPLORE}/${PATH_IMPACT}`}>Explore</a
-    >
-  </section>
-  <section class="opacity-50 grid grid-rows-[1fr_auto_auto]">
-    <div class="flex flex-col gap-y-2">
-      <Tagline>Emission scenarios</Tagline>
-      <span class="block text-xl leading-normal font-bold">
-        Select a threshold for a climate impact, explore scenarios under which
-        the threshold won't be exceeded
-      </span>
-    </div>
-    <figure class="bg-surface-weaker px-8 py-16 my-4">
-      <img
-        src="/img/emission-scenarios.png"
-        alt="Many compatible scenarios and one impact threshold"
-      />
-    </figure>
-    <span
-      class="bg-theme-weaker text-white p-5 text-center rounded-sm block"
-      aria-disabled="true">In development</span
-    >
-  </section>
+<div class="grid md:grid-cols-2 gap-y-6 gap-x-24 max-w-7xl mx-auto">
+  <Entrypoint path={PATH_IMPACT} title="what-if approach" audience={['Policy maker', 'City planners', 'Climate scientists']}>
+    <span slot="intro">
+      Starting at our current year, pick a geography<sup>1</sup>, one of the 20 indicators and explore how future<sup>2</sup> impacts will develop under 6 scenarios.
+    </span>
+    <img slot="image" src="/img/impacts.png" alt="One scenario and many resulting impacts" />
+    <ol slot="footer">
+      <li><sup>1</sup> Geographies currently available are countries, cities and exclusive economic zones. More will follow in 2024.</li>
+      <li><sup>2</sup> 12 scenarios are focusing on the years until 2100, while 12 scenarios strech until 2300.</li>
+    </ol>
+  </Entrypoint>
+  <Entrypoint path={PATH_AVOID} title="If-how approach" audience={['Policy maker', 'City planners', 'Climate scientists']}>
+    <span slot="intro"> Looking back from the future, you can select a threshold for a climate impact and explore scenarios under which the threshold won't be exceeded. </span>
+    <img slot="image" src="/img/emission-scenarios.png" alt="One scenario and many resulting impacts" />
+    <ol slot="footer">
+      <li><sup>1</sup> Geographies currently available are countries, cities and exclusive economic zones. More will follow in 2024.</li>
+      <li><sup>2</sup> 12 scenarios are focusing on the years until 2100, while 12 scenarios strech until 2300.</li>
+    </ol>
+  </Entrypoint>
+</div>
+<div class="py-20 justify-center flex-col max-w-7xl mx-auto gap-x-12 grid md:grid-cols-2">
+  <div>
+    <span class="block font-bold text-3xl mb-3">The PROVIDE project aims for a high target</span>
+    <p class="leading-normal text-xl">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum esse earum dicta iure. Neque rerum, numquam odio veniam facere consequatur itaque molestias in doloribus recusandae, at ad
+      soluta. Quisquam, beatae.
+    </p>
+  </div>
 </div>
--- src/routes/(default)/landing-page/Globe.svelte
+++ src/routes/(default)/landing-page/Globe.svelte
@@ -11,6 +11,8 @@
   import mask from '@turf/mask';
   import centroid from '@turf/centroid';
 
+  import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@rgossiaux/svelte-headlessui';
+
   export let stories;
 
   const theme = getContext('theme');
@@ -69,57 +71,61 @@
   });
 </script>
 
-<figure class="h-[70vh] grid grid-cols-5 overflow-hidden bg-theme-stronger relative">
-  <div class="col-span-3 row-start-1 col-start-1 h-[130%] bg-theme-strongest">
-    <LoadingWrapper
-      asyncProps={{ shape: $GEO_SHAPE, grid: $GEO_DATA }}
-      {process}
-      let:asyncProps
-      warningBackground={false}
-      warningInverted={true}
-    >
-      <MapProvider
-        interactive={false}
-        projection="globe"
-        style="mapbox://styles/climateanalytics/clgxes1uo00c301qy9m8ubl5e"
-        center={asyncProps.center}
-        zoomRange={[1.9, 1.9]}
-      >
+<header class="h-[70vh] max-h-[700px] grid grid-rows-6 gap-x-6 gap-y-6 grid-cols-5 overflow-hidden bg-theme-stronger relative">
+  <figure
+    aria-hidden
+    role="presentation"
+    class="col-start-1 col-span-3 absolute top-[-50%] left-[-30%] w-[130%] h-[200%] z-10 to-transparent from-theme-base"
+    style="background-image: radial-gradient(closest-side, var(--tw-gradient-from) 60%, var(--tw-gradient-to));"
+  >
+    <LoadingWrapper asyncProps={{ shape: $GEO_SHAPE, grid: $GEO_DATA }} {process} let:asyncProps warningBackground={false} warningInverted={true}>
+      <MapProvider interactive={false} projection="globe" style="mapbox://styles/climateanalytics/clqz980h0018301qwablpef7n" center={asyncProps.center} zoomRange={[2.6, 2.6]}>
         <DataSource data={asyncProps.mask}>
-          <PolygonLayer
-            before="ocean-fill"
-            fillColor={$theme.color.surface.base}
-            fill={true}
-            fillId="mask"
-            lineWidth={0.5}
-            lineColor={$theme.color.contour.base}
-          />
-          <PolygonLayer
-            before="ocean-fill"
-            lineWidth={3}
-            lineOffset={1.5}
-            lineOpacity={0.07}
-            lineColor={$theme.color.contour.base}
-          />
+          <PolygonLayer before="ocean-fill" fillColor={$theme.color.theme.stronger} fill={true} fillId="mask" lineWidth={0.5} lineColor={$theme.color.contour.base} />
+          <PolygonLayer before="ocean-fill" lineWidth={3} lineOffset={1.5} lineOpacity={0.07} lineColor={$theme.color.theme.stronger} />
         </DataSource>
         <DataSource data={asyncProps.data}>
-          <PolygonLayer
-            fill={true}
-            line={false}
-            before="mask"
-          />
+          <PolygonLayer fill={true} line={false} before="mask" />
         </DataSource>
       </MapProvider>
     </LoadingWrapper>
+  </figure>
+
+  <div class="col-span-5 row-start-1 row-span-6 col-start-1 z-20 h-1/2 self-end bg-gradient-to-t from-theme-stronger/90" aria-hidden role="presentation" />
+  <div class="col-span-5 row-start-1 row-span-6 col-start-1 z-30 flex">
+    <div class="max-w-7xl mx-auto px-6 grid grid-cols-6 grid-rows-[auto_1fr] gap-y-12 my-20">
+      <header class="col-start-1 col-span-4 text-white">
+        <h1 class="text-6xl font-semibold">A database for global-to-local climate impacts</h1>
+        <p class="text-xl">Explore data across scales and sectors. All based on the latest science</p>
+      </header>
+      <div class="col-span-3 self-center col-start-4 p-4 bg-white/10 border-white/40 border rounded-sm">
+        <span class="font-bold text-xs uppercase text-sky-100 tracking-wider mb-2 block">Sectors</span>
+        <TabGroup>
+          <TabList class="grid grid-cols-3 gap-x-8 mb-4 text-white">
+            <Tab class="flex flex-col items-start border-b-2 border-b-white">
+              <strong class="text-xl">Climate</strong>
+            </Tab>
+            <Tab class="flex flex-col items-start">
+              <strong class="text-xl">Ocean habitability</strong>
+            </Tab>
+            <Tab class="flex flex-col items-start">
+              <strong class="text-xl">Urban heat stress</strong>
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel class="flex flex-col text-white">
+              <span class="text-base mb-4">We cover data for all 190 countries in the world. Ranging from China to Lesotho. Lorem ipsum dolor something.</span>
+              <a href={currentStory.url} class="">
+                <span class="text-base text-surface-base max-w-md leading-snug">
+                  Explore, for example, how {currentStory.indicator.label} in {currentStory.geography.label} will develop under a {currentStory.scenarios[0].label} scenario.
+                </span>
+              </a>
+            </TabPanel>
+            <TabPanel>Content 2</TabPanel>
+            <TabPanel>Content 3</TabPanel>
+          </TabPanels>
+        </TabGroup>
+      </div>
+    </div>
   </div>
-  <div class="col-span-3 row-start-1 col-start-1 z-10 h-1/2 self-end bg-gradient-to-t from-theme-stronger/60" />
-  <figcaption class="pl-8 col-span-2 self-center col-start-4">
-    <p class="text-3xl text-surface-base font-bold max-w-md leading-snug mb-12">
-      How will {currentStory.indicator.label} in {currentStory.geography.emoji}&nbsp;{currentStory.geography.label} develop under a {currentStory.scenarios[0].label} scenario?
-    </p>
-    <a
-      href={currentStory.url}
-      class="bg-surface-base text-lg border text-theme-base rounded px-5 py-3">Find out more</a
-    >
-  </figcaption>
-</figure>
+</header>
