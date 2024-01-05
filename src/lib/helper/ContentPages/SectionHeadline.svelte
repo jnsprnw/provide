c3be13e33a110db61ee1a0d5b0dad0521919f53b
Jonas Parnow <6gqrbj@posteo.org>
2024-01-05 15:32:34 +0100

    Bigger font size for headings
    #281

--- src/lib/helper/ContentPages/SectionHeadline.svelte
+++ src/lib/helper/ContentPages/SectionHeadline.svelte
@@ -5,4 +5,4 @@
   const { query } = getContext('scrollContent');
 </script>
 
-<h2 id={slug} name={slug} class="scroll-mt-4 font-bold text-2xl mb-2 mt-4 first-of-type:mt-0 {query}">{title}</h2>
+<h2 id={slug} name={slug} class="scroll-mt-4 font-bold text-3xl mb-2 mt-4 first-of-type:mt-0 {query}">{title}</h2>
