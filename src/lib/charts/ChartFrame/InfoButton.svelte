465c23b0bde7237125b1077332e2ba0862052ba1
Jonas Parnow <6gqrbj@posteo.org>
2024-01-11 12:43:32 +0100

    Better chart info layout

--- src/lib/charts/ChartFrame/InfoButton.svelte
+++ src/lib/charts/ChartFrame/InfoButton.svelte
@@ -1,9 +1,5 @@
 <script>
-  import {
-    Popover,
-    PopoverButton,
-    PopoverPanel,
-  } from '@rgossiaux/svelte-headlessui';
+  import { Popover, PopoverButton, PopoverPanel } from '@rgossiaux/svelte-headlessui';
   import { createPopperActions } from 'svelte-popperjs';
   import Chevron from '$lib/helper/icons/Chevron.svelte';
 
@@ -22,24 +18,16 @@
 
 {#if items.length}
   <Popover>
-    <PopoverButton
-      use={[popperRef]}
-      class="font-bold flex text-theme-base hover:text-theme-stronger text-sm items-center"
-    >
+    <PopoverButton use={[popperRef]} class="font-bold flex text-theme-base hover:text-theme-stronger text-sm items-center">
       <span>{label}</span>
       <Chevron class="pointer-events-none stroke-theme-base" />
     </PopoverButton>
 
-    <PopoverPanel
-      use={[[popperContent, popperOptions]]}
-      class="bg-surface-base shadow-xl z-10 px-4 py-2"
-    >
-      <dl>
+    <PopoverPanel use={[[popperContent, popperOptions]]} class="bg-surface-base shadow-xl z-10 px-4 py-2">
+      <dl class="text-sm grid grid-cols-[auto_auto] gap-y-1">
         {#each items as item}
-          <div class="flex text-sm py-1">
-            <dt class="text-contour-weak mr-2">{item.label}</dt>
-            <dd class="min-w-10">{item.value}</dd>
-          </div>
+          <dt class="text-contour-weak mr-2">{item.label}</dt>
+          <dd class="min-w-10 font-bold">{item.value}</dd>
         {/each}
       </dl>
     </PopoverPanel>
