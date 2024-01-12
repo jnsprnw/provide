c388a03461dbe82762493f109a4b750bf81c41bd
Jonas Parnow <6gqrbj@posteo.org>
2024-01-12 13:28:15 +0100

    Hide embed website from search

--- src/routes/(embed)/+layout.svelte
+++ src/routes/(embed)/+layout.svelte
@@ -3,6 +3,10 @@
   import ThemeProvider from '$styles/ThemeProvider.svelte';
 </script>
 
+<svelte:head>
+  <meta name="robots" content="none, noimageindex, noarchive" />
+</svelte:head>
+
 <ThemeProvider id="light">
   <slot />
 </ThemeProvider>
