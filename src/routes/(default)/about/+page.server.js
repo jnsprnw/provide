0f75d608569dfb2c5e63f713e8fefee2df43b1bf
Jonas Parnow <6gqrbj@posteo.org>
2024-01-15 16:18:41 +0100

    Clean up

--- src/routes/(default)/about/+page.server.js
+++ src/routes/(default)/about/+page.server.js
@@ -2,7 +2,7 @@ import { loadFromStrapi } from '$utils/apis.js';
 import { generatePageTitle } from '$utils/meta.js';
 import { kebabCase } from 'lodash-es';
 import { parse } from 'marked';
-import { LABEL_ABOUT } from '$config'
+import { LABEL_ABOUT } from '$config';
 
 export const load = async ({ fetch }) => {
   const data = await loadFromStrapi('about', fetch);
