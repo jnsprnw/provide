64588d9b7e67222ce9b1f3bc977c3eb9d979275c
Jonas Parnow <6gqrbj@posteo.org>
2024-01-15 17:09:08 +0100

    Clean up, accept category prop

--- src/routes/(default)/glossary/TermSection.svelte
+++ src/routes/(default)/glossary/TermSection.svelte
@@ -6,14 +6,17 @@
   export let slug;
   export let footnote;
   export let abbreviation;
+  export let category; // This is currently not used for anything
   const { query } = getContext('scrollContent');
 </script>
 
-<header class="flex gap-x-2 items-end scroll-mt-4 mb-2 mt-4 first-of-type:mt-0 {query}" id={slug} name={slug}>
+<header class="flex gap-x-2 items-end scroll-mt-4 mb-2 mt-4 first-of-type:mt-0 {query}" id={slug} name={slug} data-category={category}>
   <h3 class="font-bold text-xl">{title}</h3>
   {#if abbreviation}<abbr {title}>{abbreviation}</abbr>{/if}
 </header>
 <HtmlContent {content} />
-{#if footnote}<footer>
-  <a href={footnote} class="underline decoration-theme-weaker hover:decoration-theme-stronger mt-3 text-sm inline-block">More information about {title}</a>
-</footer>{/if}
+{#if footnote}
+  <footer>
+    <a href={footnote} class="underline decoration-theme-weaker hover:decoration-theme-stronger mt-3 text-sm inline-block">More information about {title}</a>
+  </footer>
+{/if}
