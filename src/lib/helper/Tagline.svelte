6cac8ec24bdc4457ead6d1c3a09e55e9c42ade38
Jonas Parnow <6gqrbj@posteo.org>
2024-01-16 15:21:33 +0100

    Clean up

--- src/lib/helper/Tagline.svelte
+++ src/lib/helper/Tagline.svelte
@@ -1,10 +1,6 @@
 <script>
-  let clazz;
   export let color = 'text-theme-weaker';
   export let size = 'text-xs';
-  export { clazz as class };
 </script>
 
-<span class={`uppercase tracking-widest inline-block ${color} ${size} ${clazz}`}
-  ><slot /></span
->
+<span class={`uppercase tracking-widest inline-block ${color} ${size} ${$$props.class}`}><slot /></span>
