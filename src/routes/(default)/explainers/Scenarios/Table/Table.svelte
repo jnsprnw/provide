ec08abd79203980dc5498f364bb963a00099ee60
Jonas Parnow <6gqrbj@posteo.org>
2024-01-18 12:57:53 +0100

    New layout, styling, tooltips
    #301

--- src/routes/(default)/explainers/Scenarios/Table/Table.svelte
+++ src/routes/(default)/explainers/Scenarios/Table/Table.svelte
@@ -36,47 +36,57 @@
         'Peak <abbr title="global mean temperature">GMT</abbr>',
         'The highest global mean temperature level reached before 2100, and the year in which it is reached.',
         'gmtPeak',
-        ([value, year]) => `${value}°C in ${year}`,
+        ([value, year]) => `<strong>${value}</strong>&nbsp;°C in&nbsp;<strong>${year}</strong>`,
         ([value]) => value,
       ],
-      ['2100 <abbr title="global mean temperature">GMT</abbr>', 'Global mean temperature in the year 2100.', 'gmt2100', (value) => `${value}°C`],
+      ['2100 <abbr title="global mean temperature">GMT</abbr>', 'Global mean temperature in the year 2100.', 'gmt2100', (value) => `<strong>${value}</strong>&nbsp;°C`],
       [
-        'Cooling rate after peak',
+        'Cooling rate<br /> after peak',
         'Rate of decrease in global mean temperature after it reaches its highest point of the century, in °C per decade.',
         'coolingRateAfterPeak',
-        (value) => `${value}°C / decade`,
+        (value) => `<strong>${value}</strong>&nbsp;°C / decade`,
       ],
-      ['2050 emissions', 'Amount of greenhouse gas emissions emitted in the year 2050, expressed in gigatonnes of CO2 equivalent.', 'emissions2050', (value) => `${value} GtCO₂eq/yr`],
-      ['2100 emissions', 'Amount of greenhouse gas emissions emitted in the year 2100, expressed in gigatonnes of CO2 equivalent.', 'emissions2100', (value) => `${value} GtCO₂eq/yr`],
       [
-        'Timing of <abbr title="net zero">NZ</abbr> CO₂',
+        '2050 emissions',
+        'Amount of greenhouse gas emissions emitted in the year 2050, expressed in gigatonnes of CO2 equivalent.',
+        'emissions2050',
+        (value) => `<strong>${value}</strong>&nbsp;GtCO₂eq/yr`,
+      ],
+      [
+        '2100 emissions',
+        'Amount of greenhouse gas emissions emitted in the year 2100, expressed in gigatonnes of CO2 equivalent.',
+        'emissions2100',
+        (value) => `<strong>${value}</strong>&nbsp;GtCO₂eq/yr`,
+      ],
+      [
+        'Timing<br /> of <abbr title="net zero">NZ</abbr> CO₂',
         'Year at which CO2 emissions have been reduced to a level where remaining emissions are offset by the removal of CO2 out of the atmosphere elsewhere.',
         'timingNZCO2',
-        (value) => value,
+        (value) => `<strong>${value}</strong>`,
       ],
       [
-        'Timing of <abbr title="net zero">NZ</abbr> <abbr title="greenhouse gas emissions">GHG</abbr>',
+        'Timing<br /> of <abbr title="net zero">NZ</abbr> <abbr title="greenhouse gas emissions">GHG</abbr>',
         'Year at which greenhouse gas emissions (expressed in CO2 equivalent) have been reduced to a level where the remaining emissions are offset by the removal of emissions out of the atmosphere elsewhere.',
         'timingNZGHG',
-        (value) => value,
+        (value) => `<strong>${value}</strong>`,
       ],
       [
-        'Likelihood <abbr title="peak warming">PW</abbr> < 1.5°C',
+        'Likelihood<br /> <abbr title="peak warming">PW</abbr> < 1.5°C',
         'The likelihood of peak global mean temperature in the 21st century staying below 1.5°C.',
         'likelihood15',
-        (value) => formatValue(value, 'percent'),
+        (value) => `<strong>${formatValue(value, 'percent')}</strong>`,
       ],
       [
-        'Likelihood <abbr title="peak warming">PW</abbr> < 2°C',
+        'Likelihood<br /> <abbr title="peak warming">PW</abbr> < 2°C',
         'The likelihood of peak global mean temperature in the 21st century staying below 2°C.',
         'likelihood2',
-        (value) => formatValue(value, 'percent'),
+        (value) => `<strong>${formatValue(value, 'percent')}</strong>`,
       ],
       [
-        'Likelihood <abbr title="peak warming">PW</abbr> < 3°C',
+        'Likelihood<br /> <abbr title="peak warming">PW</abbr> < 3°C',
         'The likelihood of peak global mean temperature in the 21st century staying below 3°C.',
         'likelihood3',
-        (value) => formatValue(value, 'percent'),
+        (value) => `<strong>${formatValue(value, 'percent')}</strong>`,
       ],
     ],
     2300: [
@@ -84,23 +94,28 @@
         'Peak <abbr title="global mean temperature">GMT</abbr>',
         'The highest global mean temperature level reached before 2100, and the year in which it is reached.',
         'gmtPeak',
-        ([value, year]) => `${value}°C in ${year}`,
+        ([value, year]) => `<strong>${value}</strong>&nbsp;°C in&nbsp;<strong>${year}</strong>`,
         ([value]) => value,
       ],
-      ['2100 <abbr title="global mean temperature">GMT</abbr>', 'Global mean temperature in the year 2100.', 'gmt2100', (value) => `${value}°C`],
-      ['2300 <abbr title="global mean temperature">GMT</abbr>', 'Global mean temperature in the year 2300.', 'gmt2300', (value) => `${value}°C`],
-      ['Cooling after peak', 'Amount of decrease in global mean temperature up to 2300 after it reaches its highest point, in °C per decade.', 'coolingAfterPeak', (value) => `${value}°C`],
+      ['2100 <abbr title="global mean temperature">GMT</abbr>', 'Global mean temperature in the year 2100.', 'gmt2100', (value) => `<strong>${value}</strong>&nbsp;°C`],
+      ['2300 <abbr title="global mean temperature">GMT</abbr>', 'Global mean temperature in the year 2300.', 'gmt2300', (value) => `<strong>${value}</strong>&nbsp;°C`],
       [
-        'Timing of NZ CO2',
-        'Year at which CO2 emissions have been reduced to a level where remaining emissions are offset by the removal of CO2 out of the atmosphere elsewhere.',
-        'timingNZCO2',
-        (value) => value,
+        'Cooling<br /> after peak',
+        'Amount of decrease in global mean temperature up to 2300 after it reaches its highest point, in °C per decade.',
+        'coolingAfterPeak',
+        (value) => `<strong>${value}</strong>&nbsp;°C`,
       ],
       [
-        'Timing of NZ GHG',
+        'Timing<br /> of NZ CO2',
+        'Year at which CO2 emissions have been reduced to a level where remaining emissions are offset by the removal of CO2 out of the atmosphere elsewhere.',
+        'timingNZCO2',
+        (value) => `<strong>${value}</strong>`,
+      ],
+      [
+        'Timing<br /> of NZ GHG',
         'Year at which greenhouse gas emissions (expressed in CO2 equivalent) have been reduced to a level where the remaining emissions are offset by the removal of emissions out of the atmosphere elsewhere.',
         'timingNZGHG',
-        (value) => value,
+        (value) => `<strong>${value}</strong>`,
       ],
     ],
   };
@@ -115,10 +130,7 @@
   $: tableColumns = (COLUMNS[selectedTimeframe] ?? []).map(([label, tooltip, key, formatting = (d) => d, get = (d) => d]) => {
     const values = scenariosListed.map((s) => get(s[KEY_CHARACTERISTICS][key]));
     const domain = extent(values);
-    const scale = chroma
-      .scale([parseTailwindColor($THEME.color.surface.weakest), parseTailwindColor($THEME.color.theme.base)])
-      .domain(domain)
-      .mode('lch');
+    const scale = chroma.scale(['#fffbeb', '#fcd34d']).domain(domain).mode('lch');
     return {
       key,
       tooltip,
@@ -129,6 +141,8 @@
     };
   });
 
+  const SCENARIO_NUMBER = 'scenarioNumber';
+
   $: scenarios = scenariosListed.map((scenario, i) => {
     const { uid, label, description, isPrimary } = scenario;
     const isSelected = selectedScenarios.includes(uid);
@@ -138,8 +152,8 @@
     const values = tableColumns.map(({ key, scale, formatting, get }) => {
       const label = formatting(scenario[KEY_CHARACTERISTICS][key]);
       const value = get(scenario[KEY_CHARACTERISTICS][key]);
-      const bg = value ? scale(value).hex() : 'transparent';
-      const useBlackFont = value ? hasBlackMoreContrast(bg) : true;
+      const bg = value !== null ? scale(value).hex() : 'transparent';
+      const useBlackFont = value !== null ? hasBlackMoreContrast(bg) : true;
       return {
         label,
         value,
@@ -155,7 +169,7 @@
     );
     return {
       disabled: selectedScenarios.length >= MAX_NUMBER_SELECTABLE_SCENARIOS && !isSelected,
-      i,
+      [SCENARIO_NUMBER]: i,
       uid,
       label,
       borderColorLeft,
@@ -167,57 +181,65 @@
     };
   });
 
-  const sorting = writable('i');
+  const sorting = writable(SCENARIO_NUMBER);
   const sortingDirection = writable(1);
 
-  $: sortedScenarios = orderBy(scenarios, (s) => s[$sorting], $sortingDirection > 0 ? 'asc' : 'desc');
+  $: sortedScenarios = orderBy(scenarios, [(s) => s[$sorting], (s) => s[SCENARIO_NUMBER]], $sortingDirection > 0 ? 'asc' : 'desc');
 
   $: maxWidth = tableColumns
     .map((_, i) => {
       const length = Math.max(9 * max(scenarios.map(({ values }) => values[i]?.label?.length ?? 0)), 80);
-      return `minmax(${length}px,auto)`;
+      return `auto`;
     })
     .join(' ');
 
   // Width of the columns without the static part
   let widthColumns = 0;
+
+  $: subGridColumns = `grid-column: span ${tableColumns.length + 1};`;
 </script>
 
 <SideScrollIndicator widthOfContent={widthColumns} distanceLeft={titleWidth} distanceRight={0}>
-  <div role="treegrid" aria-rowcount={scenariosListed.length}>
-    <div role="rowgroup" class="grid max-w-full">
-      <div role="row" class="grid justify-start max-w-full grid-flow-col" style="grid-template-columns: {titleWidth}px {maxWidth};">
+  <div role="treegrid" class="grid" aria-rowcount={scenariosListed.length} style="grid-template-columns: {titleWidth}px {maxWidth};">
+    <div role="rowgroup" class="grid max-w-full justify-start grid-cols-subgrid" style={subGridColumns}>
+      <div role="row" class="grid grid-cols-subgrid" style={subGridColumns}>
         <div role="gridcell" class="sticky left-0 bg-white px-4 text-xs border-b-contour-weakest border-b flex items-end py-3">
-          <button on:click={$sorting === 'i' ? sortingDirection.update((v) => v * -1) : sorting.set('i')} class:font-bold={$sorting === 'i'}>Scenario</button>
+          <button on:click={$sorting === SCENARIO_NUMBER ? sortingDirection.update((v) => v * -1) : sorting.set(SCENARIO_NUMBER)} class:font-bold={$sorting === SCENARIO_NUMBER}>Scenario</button>
         </div>
         {#each tableColumns as { label, key, tooltip }}
           {@const isActive = $sorting === key}
-          <div role="gridcell" class="text-xs px-1 border-b-contour-weakest border-b grid grid-cols-[16px_1fr_16px] gap-x-2 items-end justify-center text-center leading-tight py-3">
-            <span
-              >{#if isActive}{#if $sortingDirection > 0}↑{:else}↓{/if}{/if}</span
+          <button
+            role="gridcell"
+            on:click={isActive ? sortingDirection.update((v) => v * -1) : sorting.set(key)}
+            class:text-theme-base={isActive}
+            class="hover:text-theme-base text-xs pl-1 pr-2 border-b-contour-weakest border-b grid grid-cols-[16px_1fr_16px] gap-x-2 items-end justify-center text-center py-3"
+          >
+            <i aria-hidden role="presentation" class="not-italic"
+              >{#if isActive}{#if $sortingDirection > 0}↓{:else}↑{/if}{/if}</i
             >
-            <button class="whitespace-nowrap" on:click={isActive ? sortingDirection.update((v) => v * -1) : sorting.set(key)} class:font-bold={isActive}>{@html label}</button>
+            <span class="whitespace-nowrap font-bold leading-tight text-current">{@html label}</span>
             <Info description={tooltip} />
-          </div>
+          </button>
         {/each}
       </div>
     </div>
-    <div role="rowgroup" class="grid max-w-full relative">
+    <div role="rowgroup" class="grid max-w-full relative grid-cols-subgrid" style={subGridColumns}>
       {#each sortedScenarios as { i, uid, label, values, borderColorLeft, description, isPrimary, disabled }, index}
         <button
           {disabled}
           aria-disabled={disabled}
           role="row"
           aria-rowindex={i}
-          class:border-b={index !== scenariosListed.length - 1}
-          class="max-w-full border-b-contour-weakest text-white focus:bg-surface-weaker focus:text-surface-weaker hover:bg-surface-weaker hover:text-surface-weaker"
+          class="max-w-full grid grid-cols-subgrid text-white focus:bg-surface-weaker focus:text-surface-weaker hover:bg-surface-weaker hover:text-surface-weaker"
+          style={subGridColumns}
           bind:clientWidth={widthColumns}
         >
-          <label for={uid} class="grid justify-start max-w-full grid-flow-col" style="grid-template-columns: {titleWidth}px {maxWidth};">
+          <label for={uid} class="grid justify-start max-w-full grid-flow-col grid-cols-subgrid" style={subGridColumns}>
             <div
               aria-disabled={disabled}
               style="border-left-color: {borderColorLeft}"
-              class="aria-disabled:cursor-not-allowed py-2 border-l-4 border-current px-3 text-left sticky left-0 bg-current grid grid-cols-[14px_auto_1fr_1rem] items-center gap-x-1.5 whitespace-nowrap overflow-hidden text-ellipsis"
+              class:border-b={index !== scenariosListed.length - 1}
+              class="border-b-contour-weakest aria-disabled:cursor-not-allowed py-2 border-l-4 border-current px-3 text-left sticky left-0 bg-current grid grid-cols-[14px_1fr_14px_14px] items-center gap-x-1.5 whitespace-nowrap overflow-hidden text-ellipsis"
               role="gridcell"
             >
               <input {disabled} aria-disabled={disabled} tabindex="-1" id={uid} type="checkbox" name="scenarios" value={uid} bind:group={selectedScenarios} />
@@ -227,18 +249,20 @@
                 class:text-contour-weakest={disabled}
                 use:tooltip={{ content: disabled ? `You can not select more than ${MAX_NUMBER_SELECTABLE_SCENARIOS} scenarios.` : undefined }}>{label}</span
               >
+              <div class="flex items-center justify-center">
+                {#if isPrimary}
+                  <Primary />
+                {/if}
+              </div>
               <Info {description} />
-
-              {#if isPrimary}
-                <Primary />
-              {/if}
             </div>
             {#each values as { label, bg, useBlackFont, value }}
               <span
                 role="gridcell"
-                class="py-3 flex items-center justify-end px-3 text-xs whitespace-nowrap"
+                class:border-b={index !== scenariosListed.length - 1}
+                class="py-3 flex items-center justify-end px-3 text-xs whitespace-nowrap border-b-white"
                 data-value={value}
-                style="background-color: {bg}; color: {useBlackFont ? '#000' : '#fff'};">{value === null ? '—' : label}</span
+                style="background-color: {bg}; color: {useBlackFont ? '#000' : '#fff'};">{@html value === null ? '—' : label}</span
               >
             {/each}
           </label>
