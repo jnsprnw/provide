90c132d3404119b65eddda5921d2f6d0ddd783c6
Jonas Parnow <6gqrbj@posteo.org>
2024-01-22 17:01:04 +0100

    Visibility
    #248

--- src/lib/helper/Spinner.svelte
+++ src/lib/helper/Spinner.svelte
@@ -2,32 +2,14 @@
   export let isLoading = true;
   export let size = 30;
   export let strokeWidth = 4;
+  export let color = 'stroke-theme-weaker';
 
   const inner = size - strokeWidth / 2;
 </script>
 
 {#if isLoading}
-  <svg
-    class="animate-spin overflow-visible"
-    viewBox="0 0 {size} {size}"
-    height={size}
-    w={size}
-  >
-    <circle
-      class="stroke-theme-weaker/20 fill-none"
-      r={inner / 2}
-      cx={size / 2}
-      cy={size / 2}
-      stroke-width={strokeWidth}
-    />
-    <circle
-      class="stroke-theme-weaker fill-none"
-      r={inner / 2}
-      cx={size / 2}
-      cy={size / 2}
-      stroke-width={strokeWidth}
-      stroke-dasharray={size * Math.PI}
-      stroke-dashoffset={((size * Math.PI) / 4) * 3}
-    />
+  <svg class="animate-spin overflow-visible" viewBox="0 0 {size} {size}" height={size} w={size}>
+    <circle class="{color}/20 fill-none" r={inner / 2} cx={size / 2} cy={size / 2} stroke-width={strokeWidth} />
+    <circle class="{color} fill-none" r={inner / 2} cx={size / 2} cy={size / 2} stroke-width={strokeWidth} stroke-dasharray={size * Math.PI} stroke-dashoffset={((size * Math.PI) / 4) * 3} />
   </svg>
 {/if}
