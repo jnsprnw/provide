3176ba13b80b673836e8db906d1a57a5a8b6494e
Jonas Parnow <6gqrbj@posteo.org>
2024-01-25 12:55:16 +0100

    New wording
    #311

--- src/routes/(default)/landing-page/Entrypoints.svelte
+++ src/routes/(default)/landing-page/Entrypoints.svelte
@@ -5,39 +5,46 @@
 
 <header class="py-20 justify-center flex-col max-w-7xl mx-auto gap-x-12 grid md:grid-cols-2">
   <div>
-    <span class="block font-bold text-3xl mb-3">One dashboard to combine opposing perspectives</span>
+    <span class="block font-bold text-3xl mb-3">Explore risks of climate overshoots across scales</span>
+    <p class="leading-normal text-xl mb-5">The PROVIDE Climate risk dashboard allows you to explore future impacts and (un)avoidable risks from climate change from cities to the global scale.</p>
     <p class="leading-normal text-xl">
-      The PROVIDE <strong>climate risk dashboard</strong> bundles data from a variety of sources. It includes the latest sciences from our partners at lorem ipsum, dolor, something, something.
+      It allows you to explore future impacts and (un)avoidable risks from climate change under scenarios of global warming. Alternatively, start local and explore what action is needed on climate to
+      avoid a specific climate impact threshold.
     </p>
-    <p class="leading-normal text-xl">It allows you to explore future impacts from climate change as the world warms. Start by picking a mode below.</p>
   </div>
 </header>
 <div class="grid md:grid-cols-2 gap-y-6 gap-x-24 max-w-7xl mx-auto">
-  <Entrypoint path={PATH_IMPACT} title="what-if approach" audience={['Policy maker', 'City planners', 'Climate scientists']}>
-    <span slot="intro">
-      Starting at our current year, pick a geography<sup>1</sup>, one of the 20 indicators and explore how future<sup>2</sup> impacts will develop under 6 scenarios.
-    </span>
+  <Entrypoint
+    path={PATH_IMPACT}
+    title="Explore future impacts"
+    audience={['Climate scientists (terrestrial, marine and urban)', 'Experts working on climate risks', 'Climate litigation experts', 'Science communicators', 'National policy makers']}
+  >
+    <div slot="intro">
+      <p class="mb-5">
+        This mode allows you to explore different climate futures and how they will affect the environment and people.: Choose your geography type from cities, countries and marine environments, and
+      </p>
+      <p>then the impact drivers/indicator to explore how future climate impacts will develop under various emission scenarios.</p>
+    </div>
     <img slot="image" src="/img/impacts.png" alt="One scenario and many resulting impacts" />
-    <ol slot="footer">
-      <li><sup>1</sup> Geographies currently available are countries, cities and exclusive economic zones. More will follow in 2024.</li>
-      <li><sup>2</sup> 12 scenarios are focusing on the years until 2100, while 12 scenarios strech until 2300.</li>
-    </ol>
   </Entrypoint>
-  <Entrypoint path={PATH_AVOID} title="If-how approach" audience={['Policy maker', 'City planners', 'Climate scientists']}>
-    <span slot="intro"> Looking back from the future, you can select a threshold for a climate impact and explore scenarios under which the threshold won't be exceeded. </span>
+  <Entrypoint path={PATH_AVOID} title="Avoid future impacts" audience={['Urban planners, policy makers and climate adaptation experts', 'Science communicators', 'Experts working on climate risks']}>
+    <div slot="intro">
+      <p class="mb-5">In this mode, you can explore how we can avoid undesirable levels of climate impacts in urban areas.</p>
+      <p>
+        You can choose a city and an urban heat indicator. Then, choose the level of impact, for example, a number of heatwave days per year, and explore when your chosen amount would be reached. You
+        can then explore under which scenario, and what climate action pathway, might help your city avoid this climate impact level.
+      </p>
+    </div>
     <img slot="image" src="/img/emission-scenarios.png" alt="One scenario and many resulting impacts" />
-    <ol slot="footer">
-      <li><sup>1</sup> Geographies currently available are countries, cities and exclusive economic zones. More will follow in 2024.</li>
-      <li><sup>2</sup> 12 scenarios are focusing on the years until 2100, while 12 scenarios strech until 2300.</li>
-    </ol>
   </Entrypoint>
 </div>
 <div class="py-20 justify-center flex-col max-w-7xl mx-auto gap-x-12 grid md:grid-cols-2">
   <div>
-    <span class="block font-bold text-3xl mb-3">The PROVIDE project aims for a high target</span>
+    <span class="block font-bold text-3xl mb-3">The PROVIDE project</span>
     <p class="leading-normal text-xl">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum esse earum dicta iure. Neque rerum, numquam odio veniam facere consequatur itaque molestias in doloribus recusandae, at ad
-      soluta. Quisquam, beatae.
+      The PROVIDE project is a Horizon Europe funded consortium of scientists and policy experts, working to develop new, faster ways to project climate impacts, and ultimately, provide information on
+      how we can avoid them. The project, aside from developing new scientific methods, aims to provide more clarity on how climate impacts might develop after peak temperatures, in so-called
+      ‘overshoot scenarios’, and if they can be reversed.
     </p>
   </div>
 </div>
