db55e49235187f2473b5a8593756d5e9981bf00b
Jonas Parnow <6gqrbj@posteo.org>
2024-01-26 14:41:56 +0100

    Clean up

--- src/routes/(default)/+page.svelte
+++ src/routes/(default)/+page.svelte
@@ -1,14 +1,12 @@
 <script>
   import Entrypoints from './landing-page/Entrypoints.svelte';
   import Globe from './landing-page/Globe.svelte';
-  import Stories from './landing-page/Stories.svelte';
 
   export let data;
 
   $: ({ stories } = data);
 </script>
 
-<!-- <Stories {stories} /> -->
 <Globe {stories} />
 
 <div class="mx-auto max-w-7xl px-6">
