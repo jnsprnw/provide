0d4046eac4371882324b2fcdb2579169961cf47d
Flavio <flavio.gortana@posteo.net>
2024-05-16 14:47:25 +0200

    fixed wrong links on adaptation page

--- src/routes/(default)/adaptation/sections/AdaptationPlanning.svelte
+++ src/routes/(default)/adaptation/sections/AdaptationPlanning.svelte
@@ -6,14 +6,15 @@
   export let subtitle;
   export let content;
   export let caseStudies;
+  $: console.log(caseStudies);
 </script>
 
 <SectionDefault {title} {subtitle} {content} />
 <h4 class="uppercase text-xs tracking-widest font-bold pt-3 mt-10 mb-3 border-t border-contour-weakest">Case studies</h4>
 <div class="grid lg:grid-cols-4 grid-cols-2 pt-5">
-  {#each caseStudies as { uid, city, description }}
-    <a href="/{PATH_ADAPTATION}/{uid}"
-      ><h4 class="font-bold text-theme-base">{city}</h4>
+  {#each caseStudies as { city, description }}
+    <a href="/{PATH_ADAPTATION}/{city.uid}"
+      ><h4 class="font-bold text-theme-base">{city.label}</h4>
       <p class="text-text-weaker text-sm">{description}</p></a
     >
   {/each}
