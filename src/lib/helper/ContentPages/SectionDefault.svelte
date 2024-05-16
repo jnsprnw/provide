938fccc8fa9bce8d7eda77f83f0d0d7b00bde8d9
Flavio <flavio.gortana@posteo.net>
2024-05-16 14:54:50 +0200

    adding slug to default section title if none is given

--- src/lib/helper/ContentPages/SectionDefault.svelte
+++ src/lib/helper/ContentPages/SectionDefault.svelte
@@ -1,5 +1,6 @@
 <script>
   import HtmlContent from '$lib/helper/HtmlContent.svelte';
+  import { kebabCase } from 'lodash-es';
   import { getContext } from 'svelte';
   export let title;
   export let subtitle;
@@ -9,7 +10,7 @@
 </script>
 
 {#if title}
-  <h2 id={slug} name={slug} class="scroll-mt-4 font-bold text-2xl mb-2 mt-4 first-of-type:mt-0 {query}">{title}</h2>
+  <h2 id={slug || kebabCase(title)} name={slug} class="scroll-mt-4 font-bold text-2xl mb-2 mt-4 first-of-type:mt-0 {query}">{title}</h2>
 {/if}
 {#if subtitle}
   <p class="text-text-weaker">{subtitle}</p>
