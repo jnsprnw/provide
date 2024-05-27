31ae7f328c25d28a1cff6d17a822b94c5e48dd5f
Flavio <flavio.gortana@posteo.net>
2024-05-27 09:56:07 +0200

    added sidescrollindicator to page intro navigation, not working thogh

--- src/lib/site/PageIntro.svelte
+++ src/lib/site/PageIntro.svelte
@@ -1,8 +1,12 @@
 <script>
+  import SideScrollIndicator from '../helper/SideScrollIndicator.svelte';
+
   export let isCompact = false;
   export let subNavigation;
   export let subNavigationLabel;
   export let backLink;
+
+  let widthContent;
 </script>
 
 <div class="bg-surface-weaker">
@@ -10,19 +14,22 @@
     {#if backLink}<a href={backLink.href} class="text-theme-base font-bol text-sm pt-3 mb-16 flex gap-1.5 font-bold"><span class="font-normal">←</span> {backLink.label}</a>{/if}
     <slot />
     {#if subNavigation}
-      <div class="border-t border-contour-weakest py-5 mt-20 flex gap-10 items-ceter">
-        {#if subNavigationLabel}
-          <p class="uppercase text-xs tracking-widest font-bold mb-4 mt-4 text-text-weaker">{subNavigationLabel}</p>
-        {/if}
-        <nav class="flex gap-10">
-          {#each subNavigation as { href, separator, abstract, label, isActive }}
-            <a {href} class="max-w-60 hover:bg-surface-weakest px-3 py-2.5 rounded-sm" class:bg-surface-weakest={isActive}
-              ><h4 class="font-bold text-theme-base mb-2">{label}</h4>
-              <p class="text-text-weake text-sm" class:text-text-weaker={!isActive}>{abstract}</p></a
-            >
-          {/each}
-        </nav>
-      </div>
+      <SideScrollIndicator widthOfContent={widthContent} distanceLeft={0} distanceRight={0}>
+        <div class="border-t border-contour-weakest py-5 mt-20 flex gap-8 items-ceter">
+          {#if subNavigationLabel}
+            <p class="uppercase text-xs tracking-widest font-bold mb-4 mt-4 text-text-weaker whitespace-nowrap">{subNavigationLabel}</p>
+          {/if}
+
+          <nav class="flex gap-10" bind:clientWidth={widthContent}>
+            {#each subNavigation as { href, separator, abstract, label, isActive }}
+              <a {href} class="max-w-60 min-w-40 hover:bg-surface-weakest px-3 py-2.5 rounded-sm" class:bg-surface-weakest={isActive}
+                ><h4 class="font-bold text-theme-base mb-2">{label}</h4>
+                <p class="text-text-weake text-sm" class:text-text-weaker={!isActive}>{abstract}</p></a
+              >
+            {/each}
+          </nav>
+        </div>
+      </SideScrollIndicator>
     {/if}
   </div>
 </div>
