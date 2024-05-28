55c55e3f9e68140b1281f2e29e23daa5a1144b30
Jonas Parnow <6gqrbj@posteo.org>
2024-05-28 14:02:01 +0200

    SideScrollIndicator fix
    The parent element needs a max width of 100vw.
    Additionally, flex layouts seem to cause problems with the calculation of the element’s width. That’s why we need separate values for widthContent and widthLabel.
    
    #361

--- src/lib/site/PageIntro.svelte
+++ src/lib/site/PageIntro.svelte
@@ -6,30 +6,33 @@
   export let subNavigationLabel;
   export let backLink;
 
-  let widthContent;
+  let widthContent = 0;
+  let widthLabel = 0;
 </script>
 
-<div class="bg-surface-weaker">
+<div class="bg-surface-weaker max-w-[100vw] overflow-hidden">
   <div class:pb-14={!subNavigation} class:gap-y-16={!subNavigation} class:pt-20={!backLink} class=" mx-auto px-2 sm:px-6 flex flex-col pt-20 {isCompact ? 'max-w-4xl' : 'max-w-7xl'}">
     {#if backLink}<a href={backLink.href} class="text-theme-base font-bol text-sm pt-3 mb-16 flex gap-1.5 font-bold"><span class="font-normal">←</span> {backLink.label}</a>{/if}
     <slot />
     {#if subNavigation}
-      <SideScrollIndicator widthOfContent={widthContent} distanceLeft={0} distanceRight={0}>
-        <div class="border-t border-contour-weakest py-5 mt-20 flex gap-8 items-ceter">
-          {#if subNavigationLabel}
-            <p class="uppercase text-xs tracking-widest font-bold mb-4 mt-4 text-text-weaker whitespace-nowrap">{subNavigationLabel}</p>
-          {/if}
+      <div class="mt-20 border-t border-contour-weakest">
+        <SideScrollIndicator widthOfContent={widthContent + widthLabel}>
+          <div class="py-5 flex gap-8 items-start">
+            {#if subNavigationLabel}
+              <span bind:clientWidth={widthLabel} class="uppercase text-xs tracking-widest font-bold mb-4 mt-4 text-text-weaker whitespace-nowrap">{subNavigationLabel}</span>
+            {/if}
 
-          <nav class="flex gap-10" bind:clientWidth={widthContent}>
-            {#each subNavigation as { href, separator, abstract, label, isActive }}
-              <a {href} class="max-w-60 min-w-40 hover:bg-surface-weakest px-3 py-2.5 rounded-sm" class:bg-surface-weakest={isActive}
-                ><h4 class="font-bold text-theme-base mb-2">{label}</h4>
-                <p class="text-text-weake text-sm" class:text-text-weaker={!isActive}>{abstract}</p></a
-              >
-            {/each}
-          </nav>
-        </div>
-      </SideScrollIndicator>
+            <nav class="flex gap-10" bind:clientWidth={widthContent}>
+              {#each subNavigation as { href, abstract, label, isActive }}
+                <a {href} class="max-w-60 min-w-48 hover:bg-surface-weakest px-3 py-2.5 rounded-sm" class:bg-surface-weakest={isActive}
+                  ><h4 class="font-bold text-theme-base mb-2">{label}</h4>
+                  <p class="text-text-weake text-sm" class:text-text-weaker={!isActive}>{abstract}</p></a
+                >
+              {/each}
+            </nav>
+          </div>
+        </SideScrollIndicator>
+      </div>
     {/if}
   </div>
 </div>
