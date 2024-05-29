20e55f2193796b07fcd7ac33705956f7cae2b28e
Flavio <flavio.gortana@posteo.net>
2024-05-29 14:48:31 +0200

    fixed margin for last section in content layout

--- src/lib/helper/ContentPages/ContentPageLayout.svelte
+++ src/lib/helper/ContentPages/ContentPageLayout.svelte
@@ -22,7 +22,7 @@
   <NestedNav contentRef={dynamicNavigation && contentRef} slot="navigation" {sections} />
   <div bind:this={contentRef}>
     {#each sections as section}
-      <section class="mt-10 pt-10 border-contour-weakest first:border-0 first:mt-0" class:border-t={section.title && !section.omitBorder} class:pt-12={section.title}>
+      <section class="mt-10 pt-10 border-contour-weakest first:border-0 first:mt-0 last:mb-12" class:border-t={section.title && !section.omitBorder} class:pt-12={section.title}>
         <svelte:component this={section.component} title={section.title} {...section.props} />
         {#each section.sections ?? [] as part}
           <svelte:component this={part.component} title={part.title} {...part.props} />
