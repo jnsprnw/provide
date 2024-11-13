8ef4cb675d06c703215ba6cf15993babbc161cf6
flavio gortana <flavio.gortana@posteo.net>
2024-11-13 17:36:26 +0100

    removed uninstalled turf modules

--- src/routes/(default)/+page.server.js
+++ src/routes/(default)/+page.server.js
@@ -1,10 +1,7 @@
-import { PATH_EXPLORE, URL_PATH_INDICATOR, URL_PATH_GEOGRAPHY, URL_PATH_SCENARIOS, URL_PATH_SCENARIO, END_IMPACT_GEO, COLOR_SCALES } from '$config';
-import qs from 'qs';
-import { loadFromAPI, loadFromStrapi, loadMetaData } from '$utils/apis.js';
+import { PATH_EXPLORE, URL_PATH_INDICATOR, URL_PATH_GEOGRAPHY, URL_PATH_SCENARIOS } from '$config';
+import { loadFromStrapi, loadMetaData } from '$utils/apis.js';
 import { buildURL } from '$utils/url.js';
-import { get, find, compact, uniq, orderBy, mapValues } from 'lodash-es';
-import { coordinatesToContours, coordinatesToRectGrid, getColorScale } from '$src/lib/utils/geo.js';
-import simplify from '@turf/simplify';
+import { get, find, compact, uniq, orderBy } from 'lodash-es';
 
 const STORIES_ORDER_GEOGRAPHY_TYPE = ['admin0', 'eez', 'cities'];
 const STORIES_ORDER_MODES = ['explore', 'avoid', 'adaptation'];
@@ -53,30 +50,6 @@ export const load = async ({ fetch }) => {
     ),
     [(d) => STORIES_ORDER_GEOGRAPHY_TYPE.indexOf(d.geographyType), (d) => STORIES_ORDER_MODES.indexOf(d.mode)]
   );
-
-  // const storiesDataRequests = stories.map(({ geography, indicator, scenarios }) => {
-  //   const query = qs.stringify({
-  //     [URL_PATH_GEOGRAPHY]: geography.uid,
-  //     [URL_PATH_INDICATOR]: indicator.uid,
-  //     [URL_PATH_SCENARIO]: scenarios[0].uid,
-  //     ...mapValues(indicator.parameters, (options) => options[0]),
-  //   });
-
-  //   return loadFromAPI(`${import.meta.env.VITE_DATA_API_URL}/${END_IMPACT_GEO}?${query}`, undefined, { indicator });
-  // });
-
-  // const storiesData = (await Promise.all(storiesDataRequests)).map(({ data, coordinatesOrigin: origin, resolution }) => {
-  //   const colorScale = getColorScale([data], COLOR_SCALES.simple);
-  //   const geoData =
-  //     resolution < 0.5
-  //       ? coordinatesToContours(data, { resolution, origin, colorScale })
-  //       : coordinatesToRectGrid(data, {
-  //           origin,
-  //           resolution,
-  //           colorScale,
-  //         });
-  //   return geoData;
-  // });
 
   return {
     stories,
