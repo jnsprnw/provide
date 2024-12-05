84f5baf023010477a2fa2222208c1670ae6b9b7c
Jonas Parnow <6gqrbj@posteo.org>
2024-12-05 22:09:25 +0100

    Use standard library, remove unused library
    #427

--- src/routes/(default)/adaptation/sections/Publications.svelte
+++ src/routes/(default)/adaptation/sections/Publications.svelte
@@ -1,12 +1,12 @@
 <script>
-  import { LABEL_ADAPTATION_RESOURCES } from '$config';
-  import { timeFormat } from 'd3-time-format';
-
   import SectionHeadline from '$src/lib/helper/ContentPages/SectionHeadline.svelte';
   export let title;
   export let publications;
 
-  const formatTime = timeFormat('%B %Y');
+  const formatDate = new Intl.DateTimeFormat('en-GB', {
+    year: 'numeric',
+    month: 'long',
+  }).format;
 </script>
 
 <div class="flex flex-col gap-y-10 pt-5 pb-10 mb-5">
@@ -22,7 +22,7 @@
           <div class="uppercase text-xs tracking-widest mb-1.5 text-text-weaker">{publication.type}</div>
           <strong class="font-bold">{publication.name}</strong>
         </div>
-        <time class="text-sm text-text-weaker" datetime={publication.date}>{formatTime(publication.date)}</time>
+        <time class="text-sm text-text-weaker" datetime={publication.date}>{formatDate(publication.date)}</time>
       </a>
     {/each}
   </div>
